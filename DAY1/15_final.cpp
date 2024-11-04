11e7dd5b779aa8db0e93ac3b5eb689a7519698fa
ecourse <smkang@codenuri.co.kr>
2024-11-04 14:50:49 +0900

    Add files via upload

---
+++ DAY1/15_final.cpp
@@ -0,0 +1,27 @@
+﻿class Base
+{
+public:
+	virtual void foo() {}
+	virtual void goo(int a) {}
+	virtual void hoo() {}
+};
+
+class Derived : public Base
+{
+public:
+	virtual void foo()      override final {} 
+								// => 더이상 override 할수 없다.
+	virtual void goo(int a) override {}
+	virtual void hoo()      override {}
+};
+
+class Derived2 : public Derived
+{
+public:
+	virtual void foo()      override {} // error
+	virtual void goo(int a) override {} // ok
+	virtual void hoo()      override {} // ok
+};
+int main()
+{
+}
