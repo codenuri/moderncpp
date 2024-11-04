d4fc3f61e0f6f79739f40c32ef48ae684ceef0e4
ecourse <smkang@codenuri.co.kr>
2024-11-04 15:48:16 +0900

    Add files via upload

--- DAY1/17_static_if1.cpp
+++ DAY1/17_static_if1.cpp
@@ -1,15 +1,35 @@
 ﻿// 51page.. if constexpr (static if)
+// 
 // 아래 코드가 왜 에러인지 생각해 보세요 ?
+/*
 class A
 {
 	int data;
 public:
 	void foo()
 	{
-		*data = 10;
+		*data = 10; // error. int 타입의 변수를 * 할수 없다.
 	}
 };
+
 int main()
 {
 	A a;
+}
+*/
+
+template<typename T>
+class A
+{
+	T data;
+public:
+	void foo()
+	{
+		*data = 10; 
+	}
+};
+
+int main()
+{
+	A<int> a;
 }
