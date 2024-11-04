282c577297f0f1a562d4ca94f920d42689c12767
ecourse <smkang@codenuri.co.kr>
2024-11-04 11:15:11 +0900

    Add files via upload

--- DAY1/5_range_for5.cpp
+++ DAY1/5_range_for5.cpp
@@ -6,9 +6,17 @@ int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
 
-	std::ranges::take_view tv(v, 3);
-	std::ranges::filter_view fv(tv, [](int n) { return n % 2 == 1});
-	std::ranges::reverse_view rv(fv);
+	// 아래 모든 코드는 "데이타의 복사본"을 만들지는 않습니다.
+	// 단지, 시각(view) 만 제공합니다
+	// 중첩이된다는 것이 핵심
+//	std::ranges::take_view tv(v, 3);
+//	std::ranges::filter_view fv(tv, [](int n) { return n % 2 == 1; });
+//	std::ranges::reverse_view rv(fv);
+
+	// 아래 코드는 위코드와 동일합니다.(간단한 테크닉으로 만든것)
+	auto rv = v | std::views::take(3)
+				| std::views::filter([](int n) { return n % 2 == 1; })
+				| std::views::reverse;
 
 
 	for (auto& n : rv) // <===
