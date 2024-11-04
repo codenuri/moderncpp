452f091589b18547dcad18832302f85249c01e6b
ecourse <smkang@codenuri.co.kr>
2024-11-04 13:48:38 +0900

    Add files via upload

---
+++ DAY1/10_user_define_literal1.cpp
@@ -0,0 +1,26 @@
+﻿// 34 page
+#include <iostream>
+
+class Meter
+{
+	int value;
+public:
+	Meter(int v) : value(v) {}
+};
+
+
+
+int main()
+{
+	Meter n = 3m;
+
+//	Gram g = 300g;
+//	auto g2 = 3kg;
+
+	auto a1 = 3.4; // double
+	auto a2 = 3.4f; // float
+}
+
+
+
+
