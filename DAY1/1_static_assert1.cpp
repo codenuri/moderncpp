8981f49e49943210f4fe3e70e90bc5723c381fd3
ecourse <smkang@codenuri.co.kr>
2024-11-04 09:32:01 +0900

    Add files via upload

---
+++ DAY1/1_static_assert1.cpp
@@ -0,0 +1,17 @@
+﻿// 7page
+#include <cassert>
+
+void foo(int age)
+{
+	// 함수 인자는 사용전에 유효성을 확인하는것이 좋습니다.
+	assert(age > 0); // C 언어에서 부터 사용하던 기술
+
+	int* p = new int[age];
+
+	delete[] p;
+}
+
+int main()
+{
+	foo(-10);
+}
