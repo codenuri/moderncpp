c71db963ba01728b2737e830f503eb76b751b58f
ecourse <smkang@codenuri.co.kr>
2024-11-04 09:33:55 +0900

    Add files via upload

--- DAY1/1_static_assert1.cpp
+++ DAY1/1_static_assert1.cpp
@@ -1,6 +1,13 @@
 ﻿// 7page
 #include <cassert>
 
+// assert : 실행시간 표현식의 유효성 조사
+// static_assert : 컴파일시간 표현식의 유효성 조사
+
+
+static_assert(sizeof(int*) >= 8, "error, not 64bit");
+
+
 void foo(int age)
 {
 	// 함수 인자는 사용전에 유효성을 확인하는것이 좋습니다.
