a97b6c0291243b1d370cbd91a12089e19c3e71bd
ecourse <smkang@codenuri.co.kr>
2024-11-04 14:53:23 +0900

    Add files via upload

--- DAY1/15_final.cpp
+++ DAY1/15_final.cpp
@@ -5,8 +5,10 @@ public:
 	virtual void goo(int a) {}
 	virtual void hoo() {}
 };
+// 가상함수 뒤에 final   : override 할수 없다는 의미
+// 클래스 이름뒤에 fianl : 상속받을수 없다 는 의미
 
-class Derived : public Base
+class Derived final : public Base
 {
 public:
 	virtual void foo()      override final {} 
