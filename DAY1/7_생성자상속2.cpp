9674c75cb3a45e2362cab09dabccd30d971e463e
ecourse <smkang@codenuri.co.kr>
2024-11-04 12:52:49 +0900

    Add files via upload

---
+++ DAY1/7_생성자상속2.cpp
@@ -0,0 +1,27 @@
+﻿#include <list>
+#include <iostream>
+
+// 기존 클래스에서 상속을 받을때
+// => 멤버 데이타를 추가하면 
+//    "추가된 멤버 데이타를 초기화" 해야 하므로 생성자를 만드는 것이 원칙
+
+class MyList : public std::list<int>
+{
+public:
+	void print() const
+	{
+		for (auto e : *this)
+			std::cout << ", ";
+		std::cout << '\n';
+	}
+};
+
+int main()
+{
+	MyList st1(10, 3); // 10개를 3으로 초기화
+	MyList st2(10);
+}
+
+
+
+
