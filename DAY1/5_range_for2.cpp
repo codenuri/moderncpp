0530b4a85a6acf48e9d14792fb7f9c47b33834b0
ecourse <smkang@codenuri.co.kr>
2024-11-04 10:55:34 +0900

    Add files via upload

---
+++ DAY1/5_range_for2.cpp
@@ -0,0 +1,39 @@
+﻿// 5_range_for1.cpp   21 page
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+	
+	for (auto& n : v) 
+	{
+		std::cout << n << std::endl;
+	}
+
+	// 위코드를 컴파일러가 아래 처럼변경합니다.
+	auto first = std::begin(v); // 또는 v.begin()
+	auto last  = std::end(v);
+
+	for (; first != last; ++first)
+	{
+		auto& e = *first;
+		//-----------------------------
+		std::cout << n << std::endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
