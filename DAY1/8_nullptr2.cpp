bbdfb1de3b161a622b23b561eece400c38ea1ece
ecourse <smkang@codenuri.co.kr>
2024-11-04 13:12:11 +0900

    Add files via upload

---
+++ DAY1/8_nullptr2.cpp
@@ -0,0 +1,36 @@
+﻿// 8_nullptr2
+#include <iostream>
+
+void foo(int* p) {}
+
+template<typename F, typename ARG>
+void forward_parameter(F f, ARG arg)	// int arg = 0 이됩니다.
+{										// f(arg) 는 에러!!
+	f(arg);
+}
+int main()
+{
+	int n = 0;
+	foo(0);	// ok
+//	foo(n);	// error. literal 0 을 포인터로 암시적 변환되지만
+			// 하지만 0으로 초기화된 변수는 포인터로 변환될수없다
+
+	forward_parameter(foo, 0); // foo(0) 으로 호출해 달라는 의도		
+								// 하지만 결국 foo(arg)가 되므로error
+
+	forward_parameter(foo, nullptr); // ok
+
+	// 결론, 함수 인자가 포인터 일때
+	//       직접 호출이 아닌 간접호출을 사용하면 0을 전달할수 없습니다.
+	//       nullptr 은 가능합니다.
+}
+
+
+
+
+
+
+
+
+
+
---
+++ DAY1/8_nullptr3.cpp
@@ -0,0 +1,26 @@
+﻿//8_nullptr3
+#include <iostream>
+
+int main()
+{
+	// literal 과 타입
+	// => 모든 값은 "타입"이 있다.
+	3;			// int
+	3.4;		// double
+	"hello";	// const char[6]
+	false;		// bool
+	nullptr;	// std::nullptr_t 타입.
+				// std::nullptr_t 타입은 모든 종류의 포인터로 암시적형변환 됩니다.
+				
+	std::nullptr_t a = nullptr;
+//	std::nullptr_t b;	// 가능하지만 사용하지는 마세요
+
+	int* p1 = a; // nullptr
+//	double* p2 = b;
+
+	void(*f)() = a;
+}
+
+
+
+
