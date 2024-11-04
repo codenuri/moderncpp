acb0ded98ca09fabde0f9715a1828d470e7b39b6
ecourse <smkang@codenuri.co.kr>
2024-11-04 13:58:23 +0900

    Add files via upload

---
+++ DAY1/10_user_define_literal2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <vector>
+using namespace std::chrono; 
+
+using namespace std::literals; // C++ 표준에서 만든 다양한 접미사!
+
+void foo(const char* s) { std::cout << "char*"; }   // 1
+void foo(std::string s) { std::cout << "string"; }  // 2
+
+int main()
+{
+	foo("hello"); // char*
+//	foo(std::string("hello")); // std::string
+	foo("hello"s); // std::string operator""s("hello")
+				
+}
+
+
