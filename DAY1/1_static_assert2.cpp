12d67cf51892fe485d965eff53bdc08daaa32f74
ecourse <smkang@codenuri.co.kr>
2024-11-04 09:59:55 +0900

    Add files via upload

--- DAY1/1_static_assert2.cpp
+++ DAY1/1_static_assert2.cpp
@@ -31,6 +31,9 @@ int main()
 
 template<typename T> void object_set_zero(T* p)
 {
+	// static_assert 는 "C++11 의 type traits" 와 같이 사용되는 경우가 많습니다.
+	// => 4일차(목)에 자세히 설명
+	// 
 	// std::is_polymorphic_v<T> : T 가 가상함수가 있으면 true
 	static_assert(!std::is_polymorphic_v<T>,
 					"error. T has virtual function");
---
+++ DAY1/2_using1.cpp
@@ -0,0 +1,15 @@
+﻿// 11 page
+// C 스타일 코드 - typedef 
+//typedef int DWORD;
+//typedef void(*F)();  // 함수 포인터 타입
+
+// C++11 using 
+// => 아래 2줄이 위에 2줄과 완벽히 동일합니다.
+using DWORD = int;
+using F = void(*)();
+
+int main()
+{
+	DWORD n; // int n
+	F     f; // void(*f)()
+}
