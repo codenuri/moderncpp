7a451f32c52aa6aebdd1600df5189052cdd11cfd
ecourse <smkang@codenuri.co.kr>
2024-11-04 13:37:15 +0900

    Add files via upload

---
+++ DAY1/9_enum.cpp
@@ -0,0 +1,19 @@
+﻿// 9_enum - 32 page
+
+enum COLOR { red = 0, green = 1, blue = 2};
+
+int main()
+{
+	int n1 = COLOR::red; // ok. 정확한 표기법
+	int n2 = red;		 // ok. COLOR 생략가능
+
+	int red = 10;
+	int n3 = red; // 변수의 red
+
+
+}
+
+
+
+
+
