b331540cd227e41dd1592b366771065adb301bad
ecourse <smkang@codenuri.co.kr>
2024-11-04 09:44:11 +0900

    Add files via upload

---
+++ DAY1/1_static_assert2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <type_traits> 
+
+#pragma pack(1)	// 구조체 align 을 1단위로
+struct PACKET	// padding 을 제거해 달라는 것
+{
+	char cmd;
+	int  data;
+};
+
+static_assert (sizeof(PACKET) == sizeof(char) + sizeof(int),
+			 "error, unexpected padding");
+
+int main()
+{
+	std::cout << sizeof(PACKET) << std::endl; // ?
+}
+
+
+
+
+
+
+template<typename T> void object_set_zero(T* p)
+{
+	memset(p, 0, sizeof(T)); 
+}
