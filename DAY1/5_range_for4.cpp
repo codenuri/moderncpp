c1aff543399904f6ba020402385647f0fa00ceaa
ecourse <smkang@codenuri.co.kr>
2024-11-04 11:19:16 +0900

    Add files via upload

--- DAY1/5_range_for4.cpp
+++ DAY1/5_range_for4.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 
 // 아래 클래스를 잘생각해보세요
-template<typename T>
-class take_view
+template<typename T> class take_view
 {
 	T& c;
 	int cnt;
@@ -13,7 +12,6 @@ public:
 	auto begin() { return c.begin(); }
 	auto end() { return c.begin() + cnt; } // 핵심!! 잘생각해 보세요
 };
-
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
@@ -21,7 +19,7 @@ int main()
 	take_view tv(v, 3);
 
 //	for (auto& n : v)
-	for (auto& n : tv)
+	for (auto& n : tv) // <===
 	{
 		std::cout << n << std::endl;
 	}
