55af0d8935819f83398c5d249e953bb95cb11f3f
ecourse <smkang@codenuri.co.kr>
2024-11-04 14:10:24 +0900

    Add files via upload

---
+++ DAY1/11_rawstring.cpp
@@ -0,0 +1,18 @@
+﻿// 11_rawstring - 36 page
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// 일반 문자열 : \ 를 특수문자 처리.
+	// => 정규 표현식, 파일 경로 등을 표기할때 \ 를 많이 사용하는데
+	//    \\ 해야 하므로 복잡해 보입니다.
+	std::string s1 = "\\\\.\\pipe\\server";
+
+	// Raw String : \ 를 일반 문자로 취급
+	std::string s2 = R"(\\.\pipe\server)";
+
+
+	std::cout << s1 << std::endl;
+	std::cout << s2 << std::endl;
+}
