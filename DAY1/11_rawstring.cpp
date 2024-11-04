122706771f1309ebe2a78a3694825bb1e9f1620c
ecourse <smkang@codenuri.co.kr>
2024-11-04 14:14:15 +0900

    Add files via upload

--- DAY1/11_rawstring.cpp
+++ DAY1/11_rawstring.cpp
@@ -10,9 +10,21 @@ int main()
 	std::string s1 = "\\\\.\\pipe\\server";
 
 	// Raw String : \ 를 일반 문자로 취급
+	// R : Raw String 이라는 것을 의미
+	// 문자열 시작 : "(
+	//        종료 : )"
 	std::string s2 = R"(\\.\pipe\server)";
-
 
 	std::cout << s1 << std::endl;
 	std::cout << s2 << std::endl;
+
+	// " 를 표기하고 싶다. => 그냥 " 사용하면 된다.
+	std::string s3 = R"(\\.\p"ipe\server)";
+
+	// 종료 표기인  )" 를 출력하려면
+	// 시작/종료 토큰 변경하세요
+	// 아래 코드
+	// 시작 : "***(
+	// 종료 : )***"
+	std::string s4 = R"***(\\.\pipe\s)"erver)***";
 }
