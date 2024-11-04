dc0d9a47a4fe713c7e2cf0de4b57e6b074fdd66f
ecourse <smkang@codenuri.co.kr>
2024-11-04 10:50:45 +0900

    Add files via upload

--- DAY1/4_beginend.cpp
+++ DAY1/4_beginend.cpp
@@ -29,6 +29,23 @@ int main()
 
 	// #4. C++20 에서 새로운 begin 나왔습니다.
 	// => visual studio 환경설정에서 C++20 으로
+	// => "std::ranges" 라는 namespace 안에 있음.
 	auto p3 = std::ranges::begin(v);
 	auto p4 = std::ranges::end(v);
+
+
+	// 왜??? 또 새로운것이 나와야 하나요 ??
+
+//	auto p5 = std::begin( std::string("hello") );
+						// 임시객체는 이문장의 끝에서 파괴 됩니다.
+						// 따라서, 반환된 p5 는 절대 사용하면 안됩니다.
+
+	auto p6 = std::ranges::begin(std::string("hello"));
+						// 컴파일에러!!
+						// 즉, 인자가 임시객체라면 에러발생
+						// 이 외에도, 다양한 특징이 있습니다.
+
 }
+
+// visual studio 2022
+// => 디폴트로 C++14 문법 사용
