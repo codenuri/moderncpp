9420e7c5979cf1306fee714fe159ec5a06e6bcf6
ecourse <smkang@codenuri.co.kr>
2024-11-04 15:03:04 +0900

    Add files via upload

--- DAY1/16_constexpr1.cpp
+++ DAY1/16_constexpr1.cpp
@@ -13,13 +13,29 @@
 int main()
 {
 	int n = 10;
-	const int c1 = 10; 
-	const int c2 = n;  
+	const int c1 = 10; // 컴파일 할때 c1값을 알수 있다.
+	const int c2 = n;  // 컴파일 할때 c2값을 알수 없다
+
+	// c1, c2 모두 값을 변경할수 없다.
 
 	// 다음중 에러를 모두 고르세요
-	int arr1[10]; 
-	int arr2[n ]; 
-	int arr3[c1]; 
-	int arr4[c2]; 
-
+	int arr1[10]; // ok
+	int arr2[n ]; // error. 
+	int arr3[c1]; // ok
+	int arr4[c2]; // error.
+	// => 배열의 크기는 컴파일 할때 값을 알아야 합니다.
+	// => 템플릿 인자도 컴파일 할때 값을 알아야 합니다.
+	// => 따라서, 같은 const 라도 될수도 있고 안될수도 있습니다.
 }
+
+// C#
+// const : 컴파일 시에 값을 알아야 한다.
+// readonly : 값을 변경할수 없다.
+
+// Rust
+// const : 컴파일 시에 값을 알아야 한다.
+// immut : 값을 변경할수 없다.
+
+// C++11
+// constexpr : 컴파일 할때 값을 알아야 한다.
+// const     : 값을 변경할수 없다.
