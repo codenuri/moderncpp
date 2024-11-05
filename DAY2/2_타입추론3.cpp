ecb8487f39da47dc53f8122418250a40514c78fb
ecourse <smkang@codenuri.co.kr>
2024-11-05 10:09:28 +0900

    Add files via upload

---
+++ DAY2/2_타입추론3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+// 규칙 2. 인자가 lvalue reference 일때( T& a)
+// => 함수 인자가 가진 "reference" 속성만 제거하고 T의  타입을 결정
+// => const, volatile 을 유지
+template<typename T> void foo(T& a)
+{
+	std::cout << __FUNCSIG__ << std::endl;
+}
+
+int main()
+{
+	int n = 10;
+	int& r = n;
+	const int c = 10;
+	const int& cr = c;
+
+	foo(n);		// T = int,			a : int& 
+	foo(c);		// T = const int	a : const int&
+	foo(r);		// T = int			a : int& 
+	foo(cr);	// T = const int	a : const int&
+}
