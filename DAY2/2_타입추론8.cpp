340ccd2f0f3714e85d7bd5d8d458a981b22e7aad
ecourse <smkang@codenuri.co.kr>
2024-11-05 11:06:37 +0900

    Add files via upload

--- DAY2/2_타입추론8.cpp
+++ DAY2/2_타입추론8.cpp
@@ -1,5 +1,6 @@
-﻿int main()
+﻿int main()    
 {
+	int n = 0;
 	int x[3] = { 1,2,3 };
 
 	// auto 와 decltype 의 차이점 #1
@@ -10,13 +11,25 @@
 
 
 	// 타입 추론과 배열이름
-	
-	decltype(x) d1;
-	
-	auto a1 = x;
-	
-	auto& a2 = x;
+	// x의 타입 : int[3]
 
+	decltype(x) d1;	// int d1[3]	즉, decltype(배열)의 결과는 배열
 	
+	auto a1 = x;	// #1. auto 를 x와 동일타입인 int[3] 로 결정했다면
+					// => int a1[3] = x; 의 코드 생성인데, error
+					// 
+					// #2. auto 를 포인터로 결정하면
+					// => int* a1 = x;  의 코드 생성인데, 에러 아님. 
+					
+					// 답은 2번. 배열을 auto 값으로 가리키면 "포인터"로추론
+	
+	auto& a2 = x;	// auto : int[3]    a2 : auto&,  int(&a2)[3]
+					// => int(&a2)[3] = x  의 코드. 에러 아님.
+					// 따라서 이 경우 auto 는 int[3]
+
+	// 정리
+	auto a3 = x;  // auto : int*		배열이 포인터로 바뀌게되는 현상
+				  //					"decay" 라고 합니다.
+	auto& a4 = x; // auto : int[3]	
 }
 
