8ec06b335dae220964fc067f44f1beac96225636
ecourse <smkang@codenuri.co.kr>
2024-11-05 10:56:49 +0900

    Add files via upload

---
+++ DAY2/2_타입추론8.cpp
@@ -0,0 +1,22 @@
+﻿int main()
+{
+	int x[3] = { 1,2,3 };
+
+	// auto 와 decltype 의 차이점 #1
+
+	decltype(x[0]) d = x[0];	// int& d = x[0]
+	
+	auto a = x[0];				// int a = x[0] 
+
+
+	// 타입 추론과 배열이름
+	
+	decltype(x) d1;
+	
+	auto a1 = x;
+	
+	auto& a2 = x;
+
+	
+}
+
