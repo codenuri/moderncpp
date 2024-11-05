dbe111e29f4b0a1270f5541e206d52a3c4ca0cef
ecourse <smkang@codenuri.co.kr>
2024-11-05 16:58:24 +0900

    Add files via upload

--- DAY2/7_move5-1.cpp
+++ DAY2/7_move5-1.cpp
@@ -36,13 +36,18 @@ int main()
 {
 	Cat c1("nabi", 2);
 
-	Cat c2 = c1;	// 복사 생성자 호출
+//	Cat c2 = c1;				// 복사 생성자 호출
+//	Cat c3 = std::move(c1);		// 이동
+//	Cat c4 = static_cast<Cat&&>(c1);	// 이동
 
-	Cat c3 = std::move(c1); 
-
-
+	Cat c5 = foo(); // 결과가 예상과 다른 이유
+					// "ellision copy" 라는 최적화 때문에. 
 }
 
+// g++ 의 "-fno-elide-constructors" 라는 옵션을 사용하면
+// => (복사, move) 생성자를 호출하지 않은 최적화를 제외할수 있습니다.
 
+// godbolt.org
+// =>위코드 붙여넣기 하세요
 
 
