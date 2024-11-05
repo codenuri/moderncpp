fbbebd7582dacf4d9897597777c9b9436bd79143
ecourse <smkang@codenuri.co.kr>
2024-11-05 13:52:52 +0900

    Add files via upload

---
+++ DAY2/4_임시객체4.cpp
@@ -0,0 +1,48 @@
+﻿// 3_임시객체1 - 77 page
+#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+// 임시객체와 함수 반환값 -  82 page
+void f1(Point  pt) {}	// call by value     : 복사본 생성
+void f2(Point& pt) {}	// call by reference : 복사본 생성 안됨
+
+/*
+int main()
+{
+	Point p(0, 0);
+	f1(p);
+	f2(p);
+}
+*/
+//===========================================
+
+
+
+
+
+
+Point pt(1, 2);
+
+Point foo()		// return by value : 임시객체를 반환
+{
+	return pt;  // 1. pt를 복사한 임시객체 생성 
+}				// 2. 임시객체 반환
+
+Point& goo()	// return by reference 
+{				// => 리턴용 임시객체를 만들지 말라.
+	return pt;
+}
+int main()
+{
+	foo().x = 10; // error. "임시객체.x = 10"
+	goo().x = 10; // ok.    "pt.x = 10"
+}
+
+
