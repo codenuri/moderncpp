5ad6c29f59181f3082b496f3cd11164f32acec06
ecourse <smkang@codenuri.co.kr>
2024-11-05 14:07:23 +0900

    Add files via upload

--- DAY2/4_임시객체6.cpp
+++ DAY2/4_임시객체6.cpp
@@ -2,7 +2,9 @@
 #include <string>
 
 // max 를 만들어 봅시다.
-int max(int a, int b)
+// => C++ 표준의 max 가 아래와 동일한 모양입니다.
+template<typename T>
+const T& Max(const T& a, const T& b)
 {
 	return a < b ? b : a;
 }
@@ -12,5 +14,11 @@ int main()
 	std::string s1 = "abc";
 	std::string s2 = "efg";
 
-	std::string s3 = max(s1, s2);
+	std::string s3 = Max(s1, s2); // ok. 단, s3는 s2의 복사본
+	const std::string& s4 = Max(s1, s2); // ok. s4는 s2의 별명
+
+	// 
+	const std::string& s5 = Max(std::string("ab"), std::string("ef"));
+
+	// s5를 사용해도 안전할까요 ?
 }
