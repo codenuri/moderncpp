77f51faff62896686de98f3e5df9ffd792713ed3
ecourse <smkang@codenuri.co.kr>
2024-11-06 10:38:25 +0900

    Add files via upload

--- DAY3/2_forwarding_reference2.cpp
+++ DAY3/2_forwarding_reference2.cpp
@@ -7,27 +7,31 @@ int main()
 
 
 	int *p = &n;
-	int* *pp = &p;
+	int* *pp = &p; // ok. 포인터의 포인터는 가능합니다.
 
 	int& r = n;
-	int& &rr = r;
+	int& &rr = r;	// error. reference 의 reference 를
+					//        "직접 코드로 작성" 할수 없다.
 
-
+	// 그런데, 타입추론이나, alias 사용시 발생하는 
+	// reference 의 reference 는 에러가 아니다. 
+	// "reference collapsing" 규칙 사용
 	using LREF = int&; 
 	using RREF = int&&;
 
 	// reference collapsing
-	LREF& r3 = ? ;
-	RREF& r4 = ? ;
-	LREF&& r5 = ? ;
-	RREF&& r6 = ?;
+	LREF& r3 = n ;	// int&  &		=> int&
+	RREF& r4 = n ;	// int&& &		=> int&
+	LREF&& r5 = n;	// int&  &&		=> int&
+	RREF&& r6 = 3;	// int&& &&		=> int&&
 }
 
 template<typename T> void foo(T a)
 {
-	T& r = a;
+	T& r = a;	// int& & r = a
+				// int& r = a;
 }
-
+foo<int&>(n); // T= int&
 
 
 
