0805b50d8cb8c387d77a9335db82cc663068e2e9
ecourse <smkang@codenuri.co.kr>
2024-11-06 14:48:43 +0900

    Add files via upload

--- DAY3/4_완벽한전달활용2.cpp
+++ DAY3/4_완벽한전달활용2.cpp
@@ -24,7 +24,13 @@ int main()
 	// 아래 코드는 동적 메모리를 몇번 할당 할까요 ?
 	// 1. new Point() 에서 한번
 	// 2. shared_ptr 생성자에서 "control block" 만들때 한번
-	std::shared_ptr<Point> sp1(new Point(1,2));
+//	std::shared_ptr<Point> sp1(new Point(1,2));
+
+
+	// #2. std::make_shared 사용
+	// 1. sizeof(Point) + sizeof(control block) 을 한번에 할당
+	// 2. 인자로 받은 값으로 Point 부분의 생성자 호출
+	std::shared_ptr<Point> sp2 = std::make_shared<Point>(1, 2);
 }
 
 
