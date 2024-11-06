705cf3ee282c7123d9dcbd1143ebc1e4a17d91d2
ecourse <smkang@codenuri.co.kr>
2024-11-06 15:26:30 +0900

    Add files via upload

--- DAY3/1_move17.cpp
+++ DAY3/1_move17.cpp
@@ -39,12 +39,24 @@ public:
 	// T&& 를 사용하는 것이 좋습니다.
 
 	template<typename T1, typename T2>
+	// T1, T2가 string과 호환되지않은 타입이면 사용하지 않을께 가 필요 
+	// => enable_if 기술.. 내일 
 	void set(T1&& n, T2&& a)
 	{
-		name = std::forward<T1>(n);
+		name    = std::forward<T1>(n);
 		address = std::forward<T2>(a);
 	}
 
+	// 생성자도 결국 "setter" 입니다.
+	// 아래 코드는 move지원 안됩니다.
+//	People(const std::string& n, const std::string& a)
+//		: name(n), address(a) {}
+	
+	template<typename T1, typename T2>
+	People(T1&& n, T2&& a)
+		: name(std::forward<T1>(n)), address(std::forward<T2>(a))
+	{
+	}
 };
 
 int main()
@@ -52,7 +64,7 @@ int main()
 	std::string name = "kim";
 	std::string addr = "seoul";
 
-	People p;
+	People p(name, std::move(addr));
 	p.set(name, addr);
 	p.set(std::move(name), addr);
 	p.set(name, std::move(addr));
