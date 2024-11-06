714c23237556c1d2c29a018a14c66279dce2c814
ecourse <smkang@codenuri.co.kr>
2024-11-06 11:07:38 +0900

    Add files via upload

--- DAY3/2_forwarding_reference4.cpp
+++ DAY3/2_forwarding_reference4.cpp
@@ -1,6 +1,30 @@
 // T&& 의 의미
 // => 복습 하실때 아래 코드를 꼭 이해 하세요
 
+
+// T&& : 임의 타입의 "lvalue, rvalue" 를 모두 받을수 있다.
+
+// 모두 받는 다는 것은 "받을수 있는 함수를 생성" 할수 있다.
+// => 즉, 한개의 함수가 아니라, 여러개 함수를 생성 한다.
+
+// 인자로 int 타입의
+// lvalue(n) 전달 : T=int&   T&&=int&   f4(int&) 
+// rvalue(3) 전달 : T=int    T&&=int&&  f4(int&&)
+// => 결국, 생성된 함수는 call by value 가 아닌 call by reference
+
+
+// int&& : rvalue reference. "int"라고 타입이 결정된 것!!
+// T&&   : ?			     템플릿 인자 "T" 사용
+
+
+// T&& 이름
+// effective c++ 책에서  "universal reference" 로 제안
+// 하지만, 표준 위원회는 "forwarding reference" 라는 이름으로 결정
+// => perfect forwarding 할때 사용하게 된다는 의미. 
+
+
+
+
 template<typename T> void f4(T&& a)
 {
 }
