7c92036c70d4f24edba8859fd91e0aead0629308
ecourse <smkang@codenuri.co.kr>
2024-11-06 16:40:52 +0900

    Add files via upload

--- DAY3/6_함수객체5.cpp
+++ DAY3/6_함수객체5.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include <algorithm>
 
+// C 언어의 qsort() 가 아래 처럼 비교 함수를 인자로 받게 됩니다.
+// 장점 : sort 의 비교정책 변경가능
+// 단점 : 느리다. 이중 루프 안에서 함수 호출
+//		 cmp1, cmp2 가 인라인 이라도 함수 포인터에 담아서 호출하므로
+//       인라인 치환될수 없다.
+
 void Sort(int* x, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
@@ -16,8 +22,8 @@ void Sort(int* x, int sz, bool(*cmp)(int, int) )
 	}
 }
 //------------------------
-bool cmp1(int a, int b) { return a < b; }
-bool cmp2(int a, int b) { return a > b; }
+inline bool cmp1(int a, int b) { return a < b; }
+inline bool cmp2(int a, int b) { return a > b; }
 
 int main()
 {
