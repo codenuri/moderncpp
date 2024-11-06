312c68645dd9dd71e794467cfb972970eee8ba3a
ecourse <smkang@codenuri.co.kr>
2024-11-06 10:48:49 +0900

    Add files via upload

--- DAY3/2_forwarding_reference3.cpp
+++ DAY3/2_forwarding_reference3.cpp
@@ -7,26 +7,28 @@ void f2(int&& r) {}
 // int&  : int 형 lvalue 만 받겠다는 의미.   "f1(3) 은 에러"
 // int&& : int 형 rvalue 만 받겠다는 의미.   "f2(n) 은 에러"
 // 
-// T&    : 
+// T&    : 임의타입의 lvalue 만 받겠다는 의미.   "f3(3) 은 에러"
 
 
 
 template<typename T> void f3(T& a)
 {
 }
-
 int main()
 {
 	int n = 3;
 
 	// #1. 타입인자를 명시적으로 전달하는 경우
-	f3<int>(? );   // T = ?     T& = ?    f3(? a)
-	f3<int&>(? );  // T = ?     T& = ?    f3(? a)
-	f3<int&&>(? ); // T = ?     T& = ?    f3(? a)
+	f3<int>(n);    // T = int     T& = int&			     f3(int& a)
+	f3<int&>(n);   // T = int&    T& = int& &  => int&   f3(int& a)
+	f3<int&&>(n);  // T = int&&   T& = int&& & => int&   f3(int& a)
 
 	// #2. 타입인자를 생략하는 경우
-	f3(3);
-	f3(n);
+	f3(3); // T 를 int, int&, int&& 중 어떠한것으로 해도 3을 받을수 없다.
+		   // error	
+
+	f3(n); // ok. n 은 int 이므로 T = int 로 결정
+		   // f3(int&) 생성. 
 }
 
 
