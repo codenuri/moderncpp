3da214950adc7a7d20d6599152fd82ec421c1726
ecourse <smkang@codenuri.co.kr>
2024-11-07 08:58:59 +0900

    Add files via upload

---
+++ DAY4/2_람다표현식2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <functional>
+
+// 람다 표현식의 원리 - 140p
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+
+	// 위 한줄을 컴파일러가 아래 처럼 변경합니다.
+
+	class CompilerGeneratedName
+	{
+	public:
+		inline auto operator()(int a, int b) const
+		{
+			return a < b;
+		}
+	};
+
+	std::sort(v.begin(), v.end(), CompilerGeneratedName{} );
+
+}
+
+
+
+
+
