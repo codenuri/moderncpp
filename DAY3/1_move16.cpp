9cb35a852024ee02708a73a851961f1b3ae32b70
ecourse <smkang@codenuri.co.kr>
2024-11-06 15:11:49 +0900

    Add files via upload

--- DAY3/1_move16.cpp
+++ DAY3/1_move16.cpp
@@ -9,9 +9,22 @@ private:
 	int age;
 
 public:
-	void set_name(const std::string& n) { name = n; }
-	void set_name(std::string&& n) { name = std::move(n); }
+	// move 지원 setter 만드는 법
+
+	// 방법 #1. 2개의 함수 사용
+//	void set_name(const std::string& n) { name = n; }
+//	void set_name(std::string&& n) { name = std::move(n); }
+
+	// 방법 #2. T&& 사용.
+	template<typename T>
+	void set_name(T&& n)
+	{
+		name = n;					// 1
+		name = std::move(n);		// 2
+		name = std::forward<T>(n);	// 3
+	}
 };
+
 int main()
 {
 	std::string s1 = "kim";
