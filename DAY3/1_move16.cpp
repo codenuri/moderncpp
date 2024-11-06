6862b641951996d219a31b6cbf7bfc9bda444977
ecourse <smkang@codenuri.co.kr>
2024-11-06 15:15:00 +0900

    Add files via upload

--- DAY3/1_move16.cpp
+++ DAY3/1_move16.cpp
@@ -19,9 +19,10 @@ public:
 	template<typename T>
 	void set_name(T&& n)
 	{
-		name = n;					// 1
-		name = std::move(n);		// 2
-		name = std::forward<T>(n);	// 3
+//		name = n;					// 1. 항상 복사
+//		name = std::move(n);		// 2. 항상 이동
+		name = std::forward<T>(n);	// 3. lvalue 를 보내면 lvalue 캐스팅(복사)
+									//    rvalue 를 보내면 rvalue 캐스팅(이동)
 	}
 };
 
@@ -32,8 +33,8 @@ int main()
 
 	People p;
 
-	p.set_name(s1);				
-	p.set_name(std::move(s2));	
+	p.set_name(s1);				// T&& : string&	
+	p.set_name(std::move(s2));	// T&& : string&&
 
 	std::cout << s1 << std::endl; // "kim"
 	std::cout << s2 << std::endl; // ""
