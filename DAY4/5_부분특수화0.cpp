acfc0ec48b12300e43421244805c6e8002bd8e79
ecourse <smkang@codenuri.co.kr>
2024-11-07 12:42:30 +0900

    Add files via upload

---
+++ DAY4/5_부분특수화0.cpp
@@ -0,0 +1,24 @@
+﻿struct Test
+{
+	static int data;
+	using DWORD = int;
+};
+int Test::data = 0;
+int p1 = 0;
+
+template<typename T> void foo(T a)
+{
+	// 아래 코드에서 * 의 의미를 생각해 보세요
+	Test::data  * p1; 
+	Test::DWORD * p2; 
+
+	
+}
+int main()
+{
+	Test t;
+	foo(t);
+}
+
+
+
