bade594aed052b6bfda67507dbee0fc552611c27
ecourse <smkang@codenuri.co.kr>
2024-11-07 09:33:59 +0900

    Add files via upload

---
+++ DAY4/2_람다표현식4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+// auto 와 람다 표현식
+int main()
+{
+	// 람다 표현식 활용
+	// #1. std::sort(), std::find() 등의 알고리즘에 인자로 전달
+	// #2. auto 변수에 담아서 함수 처럼 사용
+
+	auto f1 = [](int a, int b) { return a + b; };
+			// class xxx{ operator()(){...}}; xxx{};
+
+//	Point p = Point(0, 0); // 결국, Point p(0,0) 과 동일
+
+	int n1 = f1(1, 2);
+	int n2 = f1.operator()(1, 2);
+}
+
