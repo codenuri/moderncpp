3d806cda1eae31748261b30e44b9744250c9b376
ecourse <smkang@codenuri.co.kr>
2024-11-07 16:25:11 +0900

    Add files via upload

--- DAY4/9_enable_if8.cpp
+++ DAY4/9_enable_if8.cpp
@@ -5,15 +5,15 @@ template<typename T>
 concept pointer = std::is_pointer_v<T>;
 
 template<typename T>
-concept not_pointer = std::is_pointer_v<T>;
+concept not_pointer = !std::is_pointer_v<T>;
 
 // 아래 2개 모두 템플릿 입니다.
-void printv(const pointer auto& v)
+void printv(  const pointer auto& v)
 {
 	std::cout << v << " : " << *v << std::endl;
 }
 
-void printv(const not_pointer auto& v)
+void printv(const not_pointer  auto& v)
 {
 	std::cout << v << std::endl;
 }
