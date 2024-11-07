91bbff808b8bf2be267a00697577f8530bcc0003
ecourse <smkang@codenuri.co.kr>
2024-11-07 16:02:43 +0900

    Add files via upload

---
+++ DAY4/9_enable_if5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// if constexpr 은 C++17 에서 표준에 추가
+// => C++17 이전에는 enable_if 등의 기술로 해결
+
+template<typename T> void printv(const T& v)
+{
+	std::cout << v << " : " << *v << std::endl;
+}
+
+template<typename T> void printv(const T& v)
+{
+	std::cout << v << std::endl;
+}
+
+int main()
+{
+	int n = 10;
+
+	printv(n);
+	printv(&n);
+}
