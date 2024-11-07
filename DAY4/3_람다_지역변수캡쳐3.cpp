29027e89ea38849faa2434476b7526dfbe503a58
ecourse <smkang@codenuri.co.kr>
2024-11-07 10:38:52 +0900

    Add files via upload

--- DAY4/3_람다_지역변수캡쳐3.cpp
+++ DAY4/3_람다_지역변수캡쳐3.cpp
@@ -4,5 +4,12 @@
 
 int main()
 {
-	auto f1 = [](int a, int b) { return a + b; };
+	// 람다 표현식을 담는 3가지 방법
+	auto f1            = [](int a, int b) { return a + b; };
+	int(*f2)(int, int) = [](int a, int b) { return a + b; };
+
+	std::function<int(int, int)> f3 = [](int a, int b) { return a + b; };
+
+	// 람다표현식 여러개를 vector 에 보관하고 싶다.
+	std::vector<auto> v1; // ???
 }
