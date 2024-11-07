c3af6ea5262c678e196a59542c5af563d84b54ca
ecourse <smkang@codenuri.co.kr>
2024-11-08 08:46:45 +0900

    Add files via upload

---
+++ DAY5/1_가변인자템플릿3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+template<typename ... Ts> void foo(Ts ... args)
+{
+	// #1. parameter pack 용어 알아 두세요
+	// Ts   : int, int, int   <= template parameter pack
+	// args : 1, 2, 3		  <= function parameter pack
+
+	// #2. pack 안에 요소 갯수 확인
+	// => sizeof...(pack이름)
+	std::cout << sizeof...(Ts)   << std::endl;
+	std::cout << sizeof...(args) << std::endl;
+}
+
+
+int main()
+{
+	foo(1, 2, 3);
+}
