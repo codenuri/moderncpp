03bd01010408b636a4d1e09b456406e43ba9f425
ecourse <smkang@codenuri.co.kr>
2024-11-07 15:52:52 +0900

    Add files via upload

--- DAY4/9_enable_if3.cpp
+++ DAY4/9_enable_if3.cpp
@@ -6,9 +6,11 @@
 //			  false       "::type" 없음
 
 template<typename T> 
-				// T 가 정수면				void
+				// T 가 정수면				반환 타입은 T
 				//      정수아니면			::type 없음 - SFINAE에의해서 후보제외
-typename std::enable_if< std::is_integral_v<T>, void >::type
+//typename std::enable_if< std::is_integral_v<T>, T >::type
+
+std::enable_if_t< std::is_integral_v<T>, T >
 
 gcd(T a, T b)
 {
@@ -28,4 +30,14 @@ int main()
 	gcd(3.1, 5.4);		
 
 	gcd(3.1f, 5.4f);	
+}
+
+// 결론
+// => 템플릿을 조건을 만족할때만 사용하게 하려면
+
+// 리턴 타입 자리를 아래 처럼 변경하세요
+template<typename T> 
+std::enable_if_t<조건, 반환 타입>
+foo(T a)
+{
 }
