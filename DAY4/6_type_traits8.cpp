d85e281c24a2c4ffc96a2f9f52e272efc4c63f21
ecourse <smkang@codenuri.co.kr>
2024-11-07 14:09:09 +0900

    Add files via upload

--- DAY4/6_type_traits8.cpp
+++ DAY4/6_type_traits8.cpp
@@ -9,6 +9,17 @@ struct Object
 };
 
 // std::move 구현
+/*
+template<typename T>
+T&& xmove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+*/
+
+// 템플릿에서 인자가 T&, T&& 를 잘 구분 하세요
+// T&  : lvalue 만 받겠다는 것
+// T&& : lvalue, rvalue 모두 받겠다는 것
 
 template<typename T>
 T&& xmove(T& obj)
@@ -16,11 +27,16 @@ T&& xmove(T& obj)
 	return static_cast<T&&>(obj);
 }
 
+
 int main()
 {
 	Object o1;
 
-	Object o2 = std::move(o1);
+	Object o2 = std::move(o1);	// move
 
-	Object o3 = xmove(o2); 
+
+	Object o3 = xmove(o2);		// move
+
+	Object o4 = xmove( Object{} ); // 안되야 할까요 ?
+								   // 되야 할까요 ?
 }
