21344d177f162edece9c63fe9f4463e9b03ef82d
ecourse <smkang@codenuri.co.kr>
2024-11-07 10:17:38 +0900

    Add files via upload

--- DAY4/3_람다_지역변수캡쳐2.cpp
+++ DAY4/3_람다_지역변수캡쳐2.cpp
@@ -16,17 +16,22 @@ int main()
 
 	std::cout << v1 << std::endl; // 100
 
+
 	/*
 	class CompilerGeneratedName
 	{
-		int& v1;
-		int& v2;
+		int& v1;	// 결국 int* v1
+		int& v2;	
 	public:
 		CompilerGeneratedName(int& n1, int& n2) : v1(n1), v2(n2) {}
 
 		inline auto operator()(int a) const	
 		{
-			v1 = 100;	
+			v1 = 100;	// *v1 = 100 이므로
+						// v1을 변경한것이 아니라
+						// v1을 따라간 곳을 수정한것
+						// 따라서, 상수 함수에서도 변경가능
+
 			return a + v1 + v2;
 		}
 	};
