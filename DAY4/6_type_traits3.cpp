9da4db7cebb9231a225d4fc5a66e9e63abe7f97c
ecourse <smkang@codenuri.co.kr>
2024-11-07 13:20:19 +0900

    Add files via upload

---
+++ DAY4/6_type_traits3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// is_pointer 복사해서 is_array 만들어 보세요
+
+template<typename T> struct is_array
+{
+
+	static constexpr bool value = false; 
+};
+
+template<typename T> struct is_array<??>
+{
+	static constexpr bool value = true;
+};
+
+
+
+
+
+template<typename T> void foo(T& a)
+{
+	// is_array 만들어 보세요
+	if ( is_array<T>::value ) 
+		std::cout << "배열" << std::endl;
+	else
+		std::cout << "배열 아님" << std::endl;
+}
+int main()
+{
+	int arr[3] = { 1,2,3 };
+	foo(arr);
+}
