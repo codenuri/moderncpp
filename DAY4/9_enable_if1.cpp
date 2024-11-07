69c0ee47d0d3514cb16e38e64806f1d337b3deba
ecourse <smkang@codenuri.co.kr>
2024-11-07 15:37:13 +0900

    Add files via upload

--- DAY4/9_enable_if1.cpp
+++ DAY4/9_enable_if1.cpp
@@ -10,16 +10,21 @@ template<typename T> struct enable_if<false, T>
 {
 	// 핵심 : false 부분특수화 버전은 ::type 멤버가 없다
 };
-
 // 사용법 
 // enable_if<bool_value, 타입> 인데
 // enable_if<bool_value>       타입 생략가능. 생략시(void)
 
-
 int main()
 {
 	// 아래 코드에서 각 변수의 타입은 ?
-	enable_if<true, int>::type  n1; 
-	enable_if<true>::type       n2; 
-	enable_if<false, int>::type n3; 
+	enable_if<true, int>::type  n1; // int n1
+	enable_if<true>::type       n2; // void n2 인데, void 변수는 만들수 없다. error
+	enable_if<false, int>::type n3; // "::type" 없으므로 에러
+
+
+	// 꼭 외울것
+	enable_if<true, 타입>::type => "타입"
+	enable_if<true>     ::type = > "void"
+	enable_if<false, 타입>::type = > "::type" 없음
+	enable_if<false>     ::type = > "::type" 없음
 }
