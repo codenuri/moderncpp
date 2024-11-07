f23c5d0149c11e63517657d14be10811da2a6190
ecourse <smkang@codenuri.co.kr>
2024-11-07 15:49:15 +0900

    Add files via upload

---
+++ DAY4/9_enable_if3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <type_traits>
+//                     
+// enable_if< 조건, 타입 >::type
+//			  true		   타입
+//			  false       "::type" 없음
+
+template<typename T> 
+				// T 가 정수면				void
+				//      정수아니면			::type 없음 - SFINAE에의해서 후보제외
+typename std::enable_if< std::is_integral_v<T>, void >::type
+
+gcd(T a, T b)
+{
+
+	return 0;
+}
+
+double gcd(double, double)
+{
+	std::cout << "double\n";
+	return 0;
+}
+
+int main()
+{
+	gcd(6, 9);			
+	gcd(3.1, 5.4);		
+
+	gcd(3.1f, 5.4f);	
+}
