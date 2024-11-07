388a4291c7f1b0e850391cc0dd20c7c157562762
ecourse <smkang@codenuri.co.kr>
2024-11-07 10:44:11 +0900

    Add files via upload

---
+++ DAY4/3_람다_지역변수캡쳐4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+
+// 아래 코드에서 문제점 찾으세요!!
+int main()
+{
+	std::function<int(int, int)> f;
+
+	{
+		int n = 10;
+
+		f = [&n](int a, int b) { return a + b + n; };
+	}
+
+	int ret = f(1, 2);
+}
+
+
+
+
+
