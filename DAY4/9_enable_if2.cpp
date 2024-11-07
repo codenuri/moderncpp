082e48c8dcb8d35bba8133b9a5bf5c7162bcd7a7
ecourse <smkang@codenuri.co.kr>
2024-11-07 15:39:35 +0900

    Add files via upload

---
+++ DAY4/9_enable_if2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <type_traits>
+
+
+// 최대 공약수를 구하는 함수를 생각해 봅시다
+// => 정수 계열 타입에만 사용되어야 합니다.
+template<typename T> T gcd(T a, T b)
+{
+	static_assert(std::is_integral_v<T>, "error, T is not integer");
+	return 0;
+}
+
+int main()
+{
+	gcd(6, 9);
+	gcd(3.1,  5.4);
+}
