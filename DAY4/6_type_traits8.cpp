773c70fff6e433bf199f6a6cb074bac105138c48
ecourse <smkang@codenuri.co.kr>
2024-11-07 14:05:38 +0900

    Add files via upload

---
+++ DAY4/6_type_traits8.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <type_traits>
+
+struct Object
+{
+	Object() = default;
+	Object(const Object&) { std::cout << "copy\n"; }
+	Object(Object&&) noexcept { std::cout << "move\n"; }
+};
+
+// std::move 구현
+
+template<typename T>
+T&& xmove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+
+int main()
+{
+	Object o1;
+
+	Object o2 = std::move(o1);
+
+	Object o3 = xmove(o2); 
+}
