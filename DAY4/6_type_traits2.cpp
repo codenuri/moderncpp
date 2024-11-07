6350cff60dfcb34905844f167c8d42e393c447c4
ecourse <smkang@codenuri.co.kr>
2024-11-07 13:10:21 +0900

    Add files via upload

---
+++ DAY4/6_type_traits2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// 템플릿 인자 T가 포인터 인지 조사하는 기술
+template<typename T>
+struct is_pointer
+{
+	enum { value = false };
+};
+
+template<typename T> void foo(const T& a)
+{
+	if ( is_pointer<T>::value  )
+		std::cout << "포인터" << std::endl;
+	else
+		std::cout << "포인터 아님" << std::endl;
+}
+int main()
+{
+	int n = 0;
+	foo(n);
+	foo(&n);
+}
