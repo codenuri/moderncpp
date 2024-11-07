4e6b23efe50e060d74cc589d6040631be5edeb73
ecourse <smkang@codenuri.co.kr>
2024-11-07 09:36:20 +0900

    Add files via upload

--- DAY4/2_람다표현식4.cpp
+++ DAY4/2_람다표현식4.cpp
@@ -10,9 +10,16 @@ int main()
 	auto f1 = [](int a, int b) { return a + b; };
 			// class xxx{ operator()(){...}}; xxx{};
 
+//  auto f1 = 임시객체;
 //	Point p = Point(0, 0); // 결국, Point p(0,0) 과 동일
 
 	int n1 = f1(1, 2);
 	int n2 = f1.operator()(1, 2);
+	//------------------------------------
+	// 다음중 에러를 모두 고르세요
+	auto a1        = [](int a, int b) { return a + b; };
+	auto& a2       = [](int a, int b) { return a + b; };
+	const auto& a3 = [](int a, int b) { return a + b; };
+	auto&& a4      = [](int a, int b) { return a + b; };
 }
 
