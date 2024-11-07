06d742f7c01a7ed3938f57e0d26e8b1244024818
ecourse <smkang@codenuri.co.kr>
2024-11-07 15:31:55 +0900

    Add files via upload

---
+++ DAY4/9_enable_if1.cpp
@@ -0,0 +1,19 @@
+﻿// 6_enable_if1.cpp
+
+// 아래 코드는 이미 C++ 표준에 있습니다.(C++11)
+template<bool, typename T = void > struct enable_if
+{
+	using type = T;
+};
+
+template<typename T> struct enable_if<false, T> 
+{
+};
+
+int main()
+{
+	// 아래 코드에서 각 변수의 타입은 ?
+	enable_if<true, int>::type  n1; 
+	enable_if<true>::type       n2; 
+	enable_if<false, int>::type n3; 
+}
