16ca80e308619ab8e458771f4580e2daf1a0e3d5
ecourse <smkang@codenuri.co.kr>
2024-11-07 09:42:39 +0900

    Add files via upload

--- DAY4/2_람다표현식4.cpp
+++ DAY4/2_람다표현식4.cpp
@@ -17,9 +17,20 @@ int main()
 	int n2 = f1.operator()(1, 2);
 	//------------------------------------
 	// 다음중 에러를 모두 고르세요
-	auto a1        = [](int a, int b) { return a + b; };
-	auto& a2       = [](int a, int b) { return a + b; };
-	const auto& a3 = [](int a, int b) { return a + b; };
-	auto&& a4      = [](int a, int b) { return a + b; };
+	auto a1        = [](int a, int b) { return a + b; }; // ok 
+	auto& a2       = [](int a, int b) { return a + b; }; // error
+	const auto& a3 = [](int a, int b) { return a + b; }; // ok
+	auto&& a4      = [](int a, int b) { return a + b; }; // ok
+	
+	// 참고 : auto&& 는 rvalue reference 아닙니다.
+	//                  forwarding reference
+	auto&& a5 = 10; // ok  // int&& a5 = 10
+	auto&& a6 = n1; // ok  // int&  a6 = n1
+
+	// 다음중 좋은 코드는 ?
+	// => a1.  최적화에의해 임시객체 제거 되므로 오버헤드 없습니다.
+	// => a3 는 결국 포인터, 사용시 주소를 따라가서 접근하므로 오버헤드 있습니다
+	auto a1        = [](int a, int b) { return a + b; };  
+	const auto& a3 = [](int a, int b) { return a + b; }; 
 }
 
