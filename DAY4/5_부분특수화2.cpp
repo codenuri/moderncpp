184059adb2f47ef138f285cd03a16e5f2b4e35a3
ecourse <smkang@codenuri.co.kr>
2024-11-07 13:00:32 +0900

    Add files via upload

---
+++ DAY4/5_부분특수화2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+
+// 160 page
+
+// 부분 특수화 문법 학습을 위한 코드
+
+
+template<typename T, typename U> struct Object
+{
+	static void foo() { std::cout << "T, U" << std::endl; }
+};
+
+template<typename T, typename U> struct Object<T*, U*>
+{
+	static void foo() { std::cout << "T*, U*" << std::endl; }
+};
+
+template<typename T, typename U> struct Object<T*, U>
+{
+	static void foo() { std::cout << "T*, U" << std::endl; }
+};
+
+int main()
+{
+	// 아래 처럼 나오게 만들어 보세요.
+	Object<int,  double >::foo();// T, U
+	Object<int*, double*>::foo();// T*, U*
+	Object<int*, double> ::foo();// T*, U
+	Object<int,  int>::foo();    // T, T
+	Object<int, short>::foo();   // int, short
+}
+
