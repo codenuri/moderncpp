bb68506cab1efaf1225c1142c36bfcf60cce1788
ecourse <smkang@codenuri.co.kr>
2024-11-07 13:51:27 +0900

    Add files via upload

--- DAY4/6_type_traits4.cpp
+++ DAY4/6_type_traits4.cpp
@@ -11,6 +11,11 @@ template<typename T> struct remove_pointer
 	using type = T;
 };
 
+template<typename T> struct remove_pointer< T* >
+{
+	using type = T;
+};
+
 int main()
 {
 	remove_pointer<int*>::type n2; // int n2
@@ -18,4 +23,11 @@ int main()
 
 template<typename T> void foo(T a)
 {
+	// 주의 : type 이 dependent name 이라면 typename 필요
+	// => T에서 포인터 제거 타입
+
+	typename remove_pointer<T>::type n1;
+
+	// 아래 코드는 T에 관련 없음. typename 필요 없음
+	remove_pointer<int*>::type n2;
 }
