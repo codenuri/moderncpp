900401d89c5dd33e4a9a622ddab10c6a04173a6d
ecourse <smkang@codenuri.co.kr>
2024-11-08 08:55:33 +0900

    Add files via upload

---
+++ DAY5/1_가변인자템플릿4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int  hoo(int a) { return -a; }
+void goo(int a, int b, int c) {}
+
+template<typename ... Types> void foo(Types ... args)
+{
+	// args : 1, 2, 3
+
+	//	goo(args); // error. pack 을 직접 인자로 사용할수 없다.
+
+
+	// pack expansion
+	// => pack 안의 모든 요소를 , 를 사용해서 열거하는 기술
+	// => "pack이름"...			=> e1, e2, e3
+	// => "pack이름사용패턴"...	=> e1패턴, e2패턴, e3패턴
+
+	goo(args...);		// goo(e1, e2, e3)
+
+	goo((++args)...);	// goo(++e1, ++e2, ++e3)
+						// ()없어도 됨 "goo(++args...)"
+
+	goo(hoo(args...));
+}
+
+
+int main()
+{
+	foo(1, 2, 3);
+}
