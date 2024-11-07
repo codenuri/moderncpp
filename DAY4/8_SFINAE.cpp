8be4208ff0a81e6dcffdfb34dd501a31c150659d
ecourse <smkang@codenuri.co.kr>
2024-11-07 14:47:52 +0900

    Add files via upload

---
+++ DAY4/8_SFINAE.cpp
@@ -0,0 +1,18 @@
+﻿// 5_SFINAE - 
+#include <iostream>
+
+//template<typename T> 
+//void foo(T a)   { std::cout << "T" << std::endl; }
+
+// void foo(int a) { std::cout << "int" << std::endl; }
+
+void foo(...)   { std::cout << "..." << std::endl; }
+
+int main()
+{
+	foo(3); // 1. foo(int) 호출, exactly matching
+			// 2. foo(T) 를 사용해서 foo(int) 생성. 
+			// 3. foo(...)
+
+	// 위순서 외워 두세요.
+}
