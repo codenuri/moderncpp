537185eb27872599497d493508d150510eb1732e
ecourse <smkang@codenuri.co.kr>
2024-11-08 14:00:36 +0900

    Add files via upload

--- DAY5/7_crtp1.cpp
+++ DAY5/7_crtp1.cpp
@@ -1,15 +1,20 @@
 // 7_crtp1
 #include <iostream>
 
+template<typename T>
 class Base
 {
 public:
 	void foo()
 	{
 		// 여기서 파생클래스 이름을 사용할수 있을까 ?
+		T obj; // Derived obj
+
+		std::cout << typeid(obj).name() << std::endl;
 	}
 };
-class Derived : public Base
+
+class Derived : public Base< Derived >
 {
 };
 int main()
