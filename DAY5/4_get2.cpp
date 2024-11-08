991f2ea19e58e7008f5d0a211cb0ca02b6c2e5c0
ecourse <smkang@codenuri.co.kr>
2024-11-08 10:45:34 +0900

    Add files via upload

---
+++ DAY5/4_get2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+template<typename ... Ts>
+struct tuple
+{
+	static constexpr int N = 0;
+};
+template<typename T, typename ... Ts>
+struct tuple<T, Ts...> : public tuple<Ts...>
+{
+	using base = tuple<Ts...>;
+
+	T value;
+	tuple() = default;
+	tuple(const T& value, const Ts& ... args)
+		: value(value), base(args...) {}
+
+	static constexpr int N = base::N + 1;
+};
+
+int main()
+{
+	//	tuple<> t0;						// 보관 안함. primary template 사용
+	//	tuple<             short> t1;	// short  보관
+	//	tuple<     double, short> t2;	// double 보관
+	//	tuple<int, double, short> t3;	// int 보관
+
+	tuple<int, double, short> t3(1, 3.4, 'A');
+
+	std::cout << t3.value << std::endl; // ?? 예측
+
+	std::cout << t3.value << std::endl; // 3.4 나오게 해보세요
+
+	std::cout << t3.value << std::endl; // 'A' 나오게 해보세요
+}
