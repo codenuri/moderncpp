a43f4c90b05b70cc844ab6ac95306b6553da84af
ecourse <smkang@codenuri.co.kr>
2024-11-08 13:07:04 +0900

    Add files via upload

--- DAY5/5_invoke2.cpp
+++ DAY5/5_invoke2.cpp
@@ -1,4 +1,6 @@
-﻿class Object
+﻿#include <functional>
+
+class Object
 {
 public:
 	void foo(int code) {}		
@@ -11,8 +13,20 @@ int main()
 	void(*f1)(int) = &foo;	
 	void(Object::*f2)(int) = &Object::foo;
 
+
 	// #2. 일반 함수 포인터와 멤버 함수 포인터 사용하는 법 알아두세요
 	f1(10);       // 일반 함수 포인터 사용
 	(obj.*f2)(10);// 멤버 함수 포인터 사용
+
+	// C++ 창시자가 제안했던 "uniform call syntax" 라는 문법
+//	f1(10);
+//	f2(&obj, 10); //<== 이문법 제안
+
+
+	// 위문법은 채택되지 않았지만, 
+	// C++17 에서 아래 라이브러리 추가
+	std::invoke(f1, 10);			// f1(10)
+	std::invoke(f2, &obj, 10);		// (obj.*f2)(10)
+	
 }
 
