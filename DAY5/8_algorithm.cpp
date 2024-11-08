c46dc6ac25fc85eee83a06aed9c3b431c1cd8a75
ecourse <smkang@codenuri.co.kr>
2024-11-08 14:27:13 +0900

    Add files via upload

--- DAY5/8_algorithm.cpp
+++ DAY5/8_algorithm.cpp
@@ -9,6 +9,10 @@ int main()
 	std::vector<std::string> v = { "A", "CCC", "BB" };
 
 	// C++20 알고리즘 특징 #1. 컨테이너 전달가능
+	// => std::ranges 이름공간
+
 	std::sort(v.begin(), v.end());	// 반드시 반복자로 보내야 합니다.
+
 	std::ranges::sort(v);
+	std::ranges::sort(v.begin(), v.end());
 }
