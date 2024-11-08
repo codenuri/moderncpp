57cbcb8d488a78d3ba32eb7d97e1304fb13d6413
ecourse <smkang@codenuri.co.kr>
2024-11-08 14:02:28 +0900

    Add files via upload

--- DAY5/7_crtp1.cpp
+++ DAY5/7_crtp1.cpp
@@ -1,5 +1,10 @@
 // 7_crtp1
 #include <iostream>
+// CRTP : Curiously Recurring Template Pattern
+// => 기반 클래스에서 "파생 클래스"의 이름을 사용하는 기술
+// => 원리 template 인자로 전달
+// => C++98 시절 기술인데, 요즘 널리 사용,
+// => C++20 Range 라이브러리가 사용하는 기술
 
 template<typename T>
 class Base
