889cd12bbf90e06a7ccaa12040a8edbe95551475
ecourse <smkang@codenuri.co.kr>
2024-11-08 10:00:11 +0900

    Add files via upload

--- DAY5/2_fold3.cpp
+++ DAY5/2_fold3.cpp
@@ -5,6 +5,8 @@ template<typename ... Ts>
 void foo(std::vector<int>& v, Ts...args)
 {
 	// args 의 모든 요소를 v에 넣어 보세요(push_back)
+
+	( v.push_back(args), ... );
 }
 
 int main()
@@ -15,4 +17,5 @@ int main()
 
 	for (auto e : v)
 		std::cout << e << ", ";
+		// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 }
