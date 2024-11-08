2e4f9ab8beeefe8f500f2c1c21bb98b3eff798b1
ecourse <smkang@codenuri.co.kr>
2024-11-08 10:52:36 +0900

    Add files via upload

--- DAY5/4_get2.cpp
+++ DAY5/4_get2.cpp
@@ -27,9 +27,24 @@ int main()
 
 	tuple<int, double, short> t3(1, 3.4, 'A');
 
-	std::cout << t3.value << std::endl; // ?? 예측
+	std::cout << t3.value << std::endl; // 1
 
-	std::cout << t3.value << std::endl; // 3.4 나오게 해보세요
+	std::cout << static_cast<tuple<double, short>&>(t3).value << std::endl; // 3.4 나오게 해보세요
 
-	std::cout << t3.value << std::endl; // 'A' 나오게 해보세요
+	std::cout << static_cast<tuple<short>&>(t3).value << std::endl; // 'A' 나오게 해보세요
+
+	// get 을 생각해 봅시다.
+	auto a = get<0>(t3);
+
+	get<0>(t3) = 30;
 }
+
+template<int N, typename TP>
+튜플TP타입의 N번째 요소의 타입&
+get(TP& tp)
+{
+	return static_cast<TP의 N번째 부모 타입&>(tp).value;
+}
+
+// 위 한글을 코드로 만들면 완성!!
+// => 결국, 타입 구하기!!!
