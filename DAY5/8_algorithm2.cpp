6741a38a018e56c613191bf9b4eea0f3b550a207
ecourse <smkang@codenuri.co.kr>
2024-11-08 14:34:41 +0900

    Add files via upload

--- DAY5/8_algorithm2.cpp
+++ DAY5/8_algorithm2.cpp
@@ -14,11 +14,17 @@ int main()
 	// 사전순서가 아닌 "문자열 길이 순서"로 정렬하고 싶다.
 
 	// C++98 : 비교 정책으로 "이항 함수 전달"
+	// => 그런데, 2개의 인자를 같은 연산(size()함수) 후 비교
 	std::sort(v.begin(), v.end(), 
 			[](const std::string& s1, const std::string& s2) 
 				{ return s1.size() < s2.size(); } );
 
+	// 파이썬은 sort()의 비교 정책으로 단항함수를 받는다!!
 
-	std::ranges::sort(v);
+	// C++20 : 이항과 단항을 모두 받는다..
+	//		   이항 : 비교 방법
+	//		   단항 : projection
+	std::ranges::sort(v, std::less<int>{},
+					[](const std::string& s) { return s.size(); });	
 	
 }
