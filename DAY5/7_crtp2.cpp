90ce7257b49cd1fa14054fbe4790726eb2c6297d
ecourse <smkang@codenuri.co.kr>
2024-11-08 14:10:01 +0900

    Add files via upload

--- DAY5/7_crtp2.cpp
+++ DAY5/7_crtp2.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
+// CRTP 를 사용해서 가상함수가 아닌 코드를 가상함수 처럼 동작하게 하는 기술
+
+template<typename T>
 class Window
 {
 public:
-	void msg_loop()
+	void msg_loop()		// void msg_loop(Window* this)	
 	{
-		click();
+//		click();		//	this->click()
+
+		static_cast<T*>(this)->click();
 	}
 	void click() { std::cout << "window click\n"; }
 };
 
-class MainWindow : public Window
+class MainWindow : public Window<MainWindow>
 {
 public:
 	void click() { std::cout << "MainWindow click\n"; }
