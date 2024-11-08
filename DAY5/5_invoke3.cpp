fa09fe905756f90159db0d36d1f9ef3d0c122b8a
ecourse <smkang@codenuri.co.kr>
2024-11-08 13:14:15 +0900

    Add files via upload

--- DAY5/5_invoke3.cpp
+++ DAY5/5_invoke3.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <functional>
+
+void goo(int a, double d) {}
 
 class Object
 {
@@ -6,11 +9,23 @@ public:
 	void foo(int a, double d) {}
 };
 
-void goo(int a, double d) {}
 
-// 가변인자템플릿8에서 chronometry 복사
+
+template<typename F, typename ... Ts>
+decltype(auto) chronometry(F f, Ts&& ... args)
+{
+//	return f(std::forward<Ts>(args)...); // 일반함수만 가능
+
+	return std::invoke(f, std::forward<Ts>(args)...); // 멤버함수도 가능
+}
 
 int main()
 {
-	chronometry(goo, 1, 3.4);
+	chronometry(goo, 1, 3.4); // goo(1, 3.4)
+
+	Object obj;
+	chronometry(&Object::foo, &obj, 1, 3.4); // obj.foo(1, 3.4)
 }
+
+// 일반 함수는 "함수이름" => "함수 주소" 로 암시적 형변환 가능하지만
+// 멤버 함수는 반드시 & 연산자 필요
