48ddc205e2a2af0be74eb91acedf0c9636d2250e
ecourse <smkang@codenuri.co.kr>
2025-04-14 16:43:55 +0900

    Add files via upload

--- DAY1/16_constexpr2.cpp
+++ DAY1/16_constexpr2.cpp
@@ -7,8 +7,8 @@ int main()
 	int n = 3;
 
 	// 아래 c1, c2는 변경할수 없다는 것
-	const int c1 = n; // ok
-	const int c2 = 3; // ok
+	const int c1 = n; // ok runtime constant 
+	const int c2 = 3; // ok compile time constant
 
 	// 아래 c3, c4 는 반드시 컴파일할때 알아야 하고
 	// 변경할수도 없다는것
@@ -17,9 +17,11 @@ int main()
 
 
 	// 많이 나오는 질문
-	// "const int c3 = 리터럴" 이 컴파일 시간 상수로 
-
-	int arr1[c1];
-	int arr2[c2];
-	int arr3[c4];
+	// "const int c3 = 리터럴" 이 컴파일 시간 상수로 사용가능한데
+	// 왜 ?? constexpr 이 필요 합니까 ?
+	// => constexpr function 예제에서..
+	
+	int arr1[c1];	// error. 
+	int arr2[c2];	// ok
+	int arr3[c4];	// ok
 }
