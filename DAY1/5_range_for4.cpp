fba54cefe21a1820a0581793abf407a86ef4f35a
ecourse <smkang@codenuri.co.kr>
2025-04-14 12:24:57 +0900

    Add files via upload

---
+++ DAY1/5_range_for4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5, 6, 7, 8, 9,10 };
+
+
+	for (auto e : v)	
+	{
+		std::cout << e << ", ";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
