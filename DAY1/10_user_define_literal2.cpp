85812cb2e5b83ba20c1908f09e464445a3859675
ecourse <smkang@codenuri.co.kr>
2025-04-14 14:30:03 +0900

    Add files via upload

--- DAY1/10_user_define_literal2.cpp
+++ DAY1/10_user_define_literal2.cpp
@@ -9,7 +9,13 @@ public:
 
 // 정수형 리터럴에 붙이는 접미사는 인자가 unsigned long long 이어야 합니다.
 // => 34page 아래 표 참고
-Gram operator""gram(unsigned long long n)
+// => 또한 사용자가 만드는 리터럴은 반드시 _로 시작해야 합니다.
+//    _가 붙지 않은 것은 표준에서 사용하기로 약속됨
+// _가 없으면
+// g++ : 컴파일 에러
+// vs  : 경고 발생
+//Gram operator""gram(unsigned long long n)
+Gram operator""_gram(unsigned long long n)
 {
 	Gram g( static_cast<int>(n));
 	return g;
@@ -19,5 +25,7 @@ int main()
 {
 	// 사용자 정의 리터럴의 핵심원리
 	// => 컴파일러가 약속된 함수를 찾는 것
-	Gram g = 30gram; // operator""gram(30)
+	Gram g = 30_gram; // operator""_gram(30)
+
+	auto g2 = 30_gram;
 }
