2b513f20dbb623631fd9055e95812417bcb01eaf
ecourse <smkang@codenuri.co.kr>
2025-04-14 12:43:55 +0900

    Add files via upload

--- DAY1/5_range_for5.cpp
+++ DAY1/5_range_for5.cpp
@@ -15,8 +15,16 @@ int main()
 	// => "| 연산자" 를 연산 재정의를 사용한 코딩 테크닉일 뿐입니다.
 
 	auto rv = v | std::views::take(3) | std::views::reverse;
+			  
+	// 위처럼 | 로 하지 말고 아래 처럼했다면 어떨까요 ?
+	// C#(LINQ), Rust(Iterator 기술) 언어 가 아래처럼 사용합니다.
+	// => method chaining 이라는 기술입니다.
+	// => C++문법으로도 충분히 할수 있는데, 표준이 선택한 방식은 위 방식
+//	auto rv = v.take(3).reverse();
+
 
 	v[0] = 10;
+
 //	for (auto e : tv)
 	for (auto e : rv)
 	{
