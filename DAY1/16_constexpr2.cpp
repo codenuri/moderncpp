5f18a984ac0ef7d8f8da710b84d50e066972ceb4
ecourse <smkang@codenuri.co.kr>
2025-04-14 16:41:27 +0900

    Add files via upload

--- DAY1/16_constexpr2.cpp
+++ DAY1/16_constexpr2.cpp
@@ -14,4 +14,12 @@ int main()
 	// 변경할수도 없다는것
 	constexpr int c3 = n; // error
 	constexpr int c4 = 3; // ok
+
+
+	// 많이 나오는 질문
+	// "const int c3 = 리터럴" 이 컴파일 시간 상수로 
+
+	int arr1[c1];
+	int arr2[c2];
+	int arr3[c4];
 }
