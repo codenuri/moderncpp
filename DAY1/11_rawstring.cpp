dfdd4f3f2b3d16e70fae8bb96b29d77b2e5900f5
ecourse <smkang@codenuri.co.kr>
2025-04-14 14:49:50 +0900

    Add files via upload

--- DAY1/11_rawstring.cpp
+++ DAY1/11_rawstring.cpp
@@ -10,8 +10,22 @@ int main()
 
 	std::cout << s1 << std::endl;
 
+
 	// Raw String : '\' 를 특수문자가 아닌 일반 문자 처럼 사용해달라
 	std::string s2 = R"(\\.\pipe\server)";
 
 	std::cout << s2 << std::endl;
+
+
+	// 문자열 중간에 " 를 표기하려면
+	// => 그냥 표기하면 된다.
+	// 문자열 시작 : "(
+	// 문자열 종료 : )"
+	std::string s3 = R"(\\.\pi"pe\server)";
+
+	// 문자열 중간에 )" 를 표기하려면
+	// => 시작과 끝 토큰을 변경
+	std::string s4 = R"(\\.\pi)"pe\server)"; // error
+
+	std::string s4 = R"***(\\.\pi)"pe\server)***"; 
 }
