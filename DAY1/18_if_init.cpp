44cccb03b5c7781d84f6a7703b9c254c9a15dddd
ecourse <smkang@codenuri.co.kr>
2025-04-14 16:03:12 +0900

    Add files via upload

--- DAY1/18_if_init.cpp
+++ DAY1/18_if_init.cpp
@@ -12,8 +12,19 @@ int main()
 	}
 
 	// #2. 위 #1 은 C++17 부터 아래 처럼 사용가능합니다.
+	// legacy if :  if ( 조건식 )
+	// 새로운 if :  if ( 초기화구문; 조건식 )
 	if (int ret1 = foo(); ret1 == 0)
 	{
+
+	}	//<== ret1 파괴!! 다음 문장부터는 다시 ret1 선언 가능
+
+
+	// switch 도 가능합니다.
+	switch (int n = foo(); n)
+	{
+	case 1: break;
+	case 2: break;
 
 	}
 
