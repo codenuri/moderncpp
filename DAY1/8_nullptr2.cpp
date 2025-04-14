3461263f5ca62523159f8c24008a5c89e756f961
ecourse <smkang@codenuri.co.kr>
2025-04-14 13:33:39 +0900

    Add files via upload

--- DAY1/8_nullptr2.cpp
+++ DAY1/8_nullptr2.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 template<typename F, typename ARG>
-void forward_parameter(F f, ARG arg)
+void forward_parameter(F f, ARG arg)	// int arg = 0; 
 {
-	f(arg);
+	f(arg);	// arg 가 int 였다면 foo(arg) 이므로 error
 }
 
 
@@ -20,9 +20,15 @@ int main()
 
 	// #2. 아래 코드를 생각해 보세요
 
-	forward_parameter(foo, 0); // foo 에게 0을 전달해 달라
+///	forward_parameter(foo, 0); // foo 에게 0을 전달해 달라
 								// 즉, foo(0) 해달라는 것
+								// 하지만 지금은 에러.
+	forward_parameter(foo, nullptr); // ok!!!
 
+	// nullptr 을 만든이유
+	// => C++11 부터 "인자를 forward"하는 기술을 널리 사용하는데
+	// => 0 사용시 위처럼 에러가 발생할수 있다.
+	// => nullptr 사용시 문제 없음. 
 }
 
 
