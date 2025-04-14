84fe1ce8ef03b48d9efc5f65cfa943eeef256832
ecourse <smkang@codenuri.co.kr>
2025-04-14 15:56:10 +0900

    Add files via upload

--- DAY1/15_final.cpp
+++ DAY1/15_final.cpp
@@ -10,11 +10,14 @@ class Derived : public Base
 {
 public:
 	// final : 더이상 override 할수 없다.
+	// => 라이브러리 내부에서만 override 하고
+	// => 라이브러리 사용자는 override 할수 없게 할때 많이 사용
 	void foo()      override final {} 
 	void goo(int a) override {}
 	void hoo()      override {}
 };
-class Derived2 : public Derived
+// 클래스 이름뒤에 final : 상속받을수 없다.
+class Derived2 final : public Derived
 {
 public:
 	void foo()      override final {} // error. final!!
@@ -22,6 +25,8 @@ public:
 	void hoo()      override {} // ok
 };
 
+class Derived3 : public Derived2 {}; // error
+									// Derived2 는 final 클래스
 
 int main()
 {
