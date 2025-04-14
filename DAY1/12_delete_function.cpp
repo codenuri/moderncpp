4284eb04c81768b467c73f4d40d72efacebe5745
ecourse <smkang@codenuri.co.kr>
2025-04-14 14:53:00 +0900

    Add files via upload

---
+++ DAY1/12_delete_function.cpp
@@ -0,0 +1,25 @@
+// 12_delete_function 37 page
+
+class Point
+{
+	int x, y;
+public:
+	Point() {}
+	Point(int a, int b) {}
+
+	// 아래 6개 함수를 "special member function" 이라고 합니다.
+	// => 사용자가 만들지 않으면 컴파일러가 제공합니다.
+	// 
+	// => 디폴트 생성자, 소멸자, 복사 생성자, 이동생성자
+	//    복사 대입 연산자, 이동 대입 연산자
+
+	// 컴파일러에게 만들지 못하게 하려면 "삭제" 하면됩니다.
+	Point(const Point&) = delete; // C++11 함수 삭제 문법
+
+};
+int main()
+{
+	Point p1;
+	Point p2(1, 2);
+	Point p3(p2); // 컴파일러가 만들어주는 복사생성자사용
+}
