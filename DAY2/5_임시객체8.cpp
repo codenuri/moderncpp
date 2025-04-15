af4436fc477cf1664c8189a8a44c56ee0d23d80c
ecourse <smkang@codenuri.co.kr>
2025-04-15 17:03:48 +0900

    Add files via upload

---
+++ DAY2/5_임시객체8.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+// 임시객체의 어려움을 보여주는 예제
+
+// Max 를 만들어 봅시다.
+
+int main()
+{
+	std::string s1 = "AAA";
+	std::string s2 = "BBB";
+
+	std::string s3 = Max(s1, s2);
+}
