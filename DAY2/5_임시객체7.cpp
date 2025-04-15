44b8ed9ae9f50343a2421482f0ee409c59c7f6b4
ecourse <smkang@codenuri.co.kr>
2025-04-15 16:49:36 +0900

    Add files via upload

--- DAY2/5_임시객체7.cpp
+++ DAY2/5_임시객체7.cpp
@@ -11,9 +11,18 @@ public:
 	Counter increment()
 	{
 		++cnt;
-
 		return *this;
 	}
+
+	Counter() = default;
+
+	~Counter() { std::cout << cnt << " 를 가진 객체 파괴\n"; }
+
+	Counter(const Counter& other) : cnt(other.cnt)
+	{
+		std::cout << cnt << " 를 가진 객체를 복사해서 객체 생성\n";
+	}
+
 };
 
 int main()
@@ -22,7 +31,9 @@ int main()
 
 	// 아래 처럼 멤버 함수를 연속으로 호출하는 것을
 	// => method chaining 이라고 하고 요즘 많은 언어에서 유행합니다.
+	std::cout << "------------------------------------\n";
 	c.increment().increment().increment();
+	std::cout << "------------------------------------\n";
 
 	std::cout << c.cnt << std::endl;
 }
