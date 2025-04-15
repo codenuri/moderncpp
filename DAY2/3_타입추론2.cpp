4c502d16bd59dd746d34b7b12abb99b26494f353
ecourse <smkang@codenuri.co.kr>
2025-04-15 12:25:28 +0900

    Add files via upload

--- DAY2/3_타입추론2.cpp
+++ DAY2/3_타입추론2.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 // 템플릿사용시 추론된 타입을 확인하려면
 // 방법 #1. godbolt.org 에서 어셈블리 코드 보세요 - 권장
+// 방법 #2. 함수 이름(signature 포함)출력
 
 template<typename T> void foo(T a)
 {
-
+	// __func__ : 함수 이름을 담은 매크로 (C++ 표준)
+	std::cout << __func__ std::endl;
 }
+
+
 int main()
 {
 	// #1. 타입을 명시적으로 전달하는 경우
