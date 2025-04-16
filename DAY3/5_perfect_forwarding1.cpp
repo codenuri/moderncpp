b005f90b970864da05c0141a5fe10ae6b230b48c
ecourse <smkang@codenuri.co.kr>
2025-04-16 15:52:27 +0900

    Add files via upload

---
+++ DAY3/5_perfect_forwarding1.cpp
@@ -0,0 +1,23 @@
+// 7_완벽한전달1 - 113 page
+#include <iostream>
+
+void foo(int a)  {}
+void goo(int& a) { a = 100; }
+
+// 성능측정하는 함수를 만들어 봅시다.
+template<typename F, typename T>
+void chronometry(F f, T arg)
+{
+	// 현재시간기록
+	f(arg);
+	// 수행시간 출력
+}
+int main()
+{
+	int n = 10;
+
+	chronometry(foo, 10);	// foo(10);
+	chronometry(goo, n);	// goo(n);
+
+	std::cout << n << std::endl;
+}
