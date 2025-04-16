841e33aaea5ca3fe82e88e5a2a25b8c8a7254cce
ecourse <smkang@codenuri.co.kr>
2025-04-16 10:39:20 +0900

    Add files via upload

--- DAY3/3_move04.cpp
+++ DAY3/3_move04.cpp
@@ -12,12 +12,31 @@ public:
 	}
 	~Cat() { delete[] name; }
 
+
 	Cat(const Cat& c) : age(c.age)
 	{
 		name = new char[strlen(c.name) + 1];
 
 		strcpy_s(name, strlen(c.name) + 1, c.name);
 	}
+
+	// 임시객체(rvalue) 전용 "복사 생성자"
+	// => 위 복사 생성자가 "lvalue, rvalue" 를 모두 받을수있지만
+	// => 아래와 같이 rvalue 전용 버전을 만들면
+	// => 임시객체(rvalue) 의 경우 아래 버전 사용
+	Cat(Cat&& c) : age(c.age)
+	{
+		name = c.name;
+
+		c.name = nullptr; // 자원을 포기하게 합니다.
+						  // 임시객체 소멸자가 delete 0 하게 되는데
+						  // 0번지를 delete 하는 것은 아무일도 하지 않는것!!
+
+						  // 이작업을 하려면 "임시객체(rvalue)" 를
+						  // "상수성없이(non-const) 가르켜야 합니다."
+	}
+
+
 };
 
 Cat foo()
@@ -26,6 +45,8 @@ Cat foo()
 
 	return c;
 }
+
+
 
 int main()
 {
