acfc88053f852ab36938ffcf48b3b4dfb97231f9
ecourse <smkang@codenuri.co.kr>
2025-04-16 13:52:41 +0900

    Add files via upload

--- DAY3/3_move12.cpp
+++ DAY3/3_move12.cpp
@@ -20,7 +20,11 @@ public:
 	// => C++98 시절은 best!!
 	// => C++11 이후는 나쁘지는 않지만 최선은 아님
 	// => 이유 : move 지원 안됨
-	void set_name(const std::string& n) { name = n; }
+	void set_name(const std::string& n) { name = n; } // 항상 복사
+	void set_name(const std::string& n) { name = std::move(n); } 
+													// 역시 항상 복사
+													// 상수는 이동될수 없다.
+								// 해결책은 다음소스
 };
 
 int main()
@@ -30,8 +34,10 @@ int main()
 
 	People p;
 
-	p.set_name(s1);
-	p.set_name(std::move(s2));	
+
+
+	p.set_name(s1);				// s1 자원 복사하라는 의도, s1은 계속 사용할것이다
+	p.set_name(std::move(s2));	// s2 자원 이동하라는 의도
 
 	std::cout << s1 << std::endl; // "kim"
 	std::cout << s2 << std::endl; // ""
