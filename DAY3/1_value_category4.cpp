9d2436afe3e9e238e6d01886f2dad8bf9e8c299a
ecourse <smkang@codenuri.co.kr>
2025-04-16 09:13:01 +0900

    Add files via upload

--- DAY3/1_value_category4.cpp
+++ DAY3/1_value_category4.cpp
@@ -12,14 +12,22 @@ int main()
 {
 	int n = 0;
 
-	VALUE_CATEGORY(n);
+	VALUE_CATEGORY(n);		// lvalue
 	VALUE_CATEGORY(n+2);
-	VALUE_CATEGORY(++n);
+	VALUE_CATEGORY(++n);	// lvalue
 	VALUE_CATEGORY(n++);
 	VALUE_CATEGORY(10);
 
 	std::string s = "AA";
-	VALUE_CATEGORY(s);
-	VALUE_CATEGORY(std::string{"aa"}); // 임시객체 
+	VALUE_CATEGORY(s);		// lvalue
+	VALUE_CATEGORY(std::string{"aa"}); // 임시객체, rvalue
 
+
+	VALUE_CATEGORY(10);		// 정수, 실수 리터럴은 rvalue	
+	VALUE_CATEGORY("AAA");	// 문자열 리터럴만 lvalue
+							// => 문자열 리터럴은 코드메모리가아닌
+							// => 데이타 메모리(상수메모리) 사용
+
+//	&10;    // error
+	&"AAA"; // ok
 }
