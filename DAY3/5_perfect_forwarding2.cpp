4059036bed1e3c1eb2b951df213edf9842b99fd7
ecourse <smkang@codenuri.co.kr>
2025-04-16 16:01:44 +0900

    Add files via upload

--- DAY3/5_perfect_forwarding2.cpp
+++ DAY3/5_perfect_forwarding2.cpp
@@ -6,6 +6,9 @@ void goo(int& a) { a = 100; }
 // 1. call by reference 로 해야 하고
 // 2. lvalue reference, rvalue reference 버전을 각각 제공해야 한다.
 
+// => 아래 코드는 한가지 문제점이 있습니다.
+// => 다음소스 참고
+
 template<typename F> void chronometry(F f, int& arg)
 {
 	f(arg);
