2f25cadf54a55fdb51d9cc4ec8da61ae4de49713
ecourse <smkang@codenuri.co.kr>
2025-04-17 13:43:11 +0900

    Add files via upload

--- DAY4/reference.cpp
+++ DAY4/reference.cpp
@@ -6,11 +6,19 @@ void f2(const std::string& s) {} // good, 복사본 없음
 
 // 다음중 좋은 코드는 ?
 void f3(int n)        { int k = n; }
+
+
+
+
+
+
 void f4(const int& n) { int k = n; }
+					//  int k = (*n)
+
 
 int main()
 {
 	int a = 10;
 	f3(a);
-	f3(b);
+	f3(a);
 }
