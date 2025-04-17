629b463154f9cd10203d1be92830b0ccbde1ecc8
ecourse <smkang@codenuri.co.kr>
2025-04-17 10:35:59 +0900

    Add files via upload

--- DAY4/3_함수객체3-1.cpp
+++ DAY4/3_함수객체3-1.cpp
@@ -4,11 +4,25 @@
 
 bool foo(int n) { return n % 3 == 0; }
 
+
+struct IsDivide
+{
+	int value;
+	
+	IsDivide(int n) : value(n) {}
+
+	bool operator()(int n)  { return n % value == 0; }
+};
+
 int main()
 {
 	std::vector<int> v = { 1,2,6,5,4,3,7,8 };
 
 	int k = 3;
+
+	IsDivide f(k); // f 는 내부적으로 k 값을 캡쳐(보관) 합니다.
+	int ret = f(10); // ?
+
 	auto p2 = std::find_if(v.begin(), v.end(), ? );
 }
 
