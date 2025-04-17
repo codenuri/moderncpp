a409acd72446aafa330a68355ca45d2a1698af6d
ecourse <smkang@codenuri.co.kr>
2025-04-18 08:48:55 +0900

    Add files via upload

--- DAY5/1_specialization1.cpp
+++ DAY5/1_specialization1.cpp
@@ -25,6 +25,6 @@ public:
 int main()
 {
 	Stack<int>   s1; s1.push(0);
-	Stack<int*>  s2; s2.push(0);
-	Stack<char*> s3; s3.push(0);
+//	Stack<int*>  s2; s2.push(0);
+//	Stack<char*> s3; s3.push(0);
 }
