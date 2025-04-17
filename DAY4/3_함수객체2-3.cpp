94a4f51c7c3d22842ad7bed8d4277785fd720fdc
ecourse <smkang@codenuri.co.kr>
2025-04-17 10:28:20 +0900

    Add files via upload

--- DAY4/3_함수객체2-3.cpp
+++ DAY4/3_함수객체2-3.cpp
@@ -13,27 +13,24 @@ public:
 	}
 	int operator()()
 	{
+		if (bs.none()) // 모든 비트가 0 이면
+		{
+			if (recycle == true)
+				bs.set(); // 다시 모든 비트를 1로
+			else
+				return -1;
+		}
+
 		int k = -1;
-
-		while (!bs.test(k = rand() % 10));
-
+		while ( !bs.test( k = rand() % 10 ) );
 		bs.reset(k);
-
 		return k;
 	}
 };
-
-
-
 URandom urand;	
-
-
-
-
-
 int main()
 {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 15; i++)
 	{
 		std::cout << urand() << std::endl;
 	}
