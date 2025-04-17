6b7cc10f753c9aa2323ff5b0dd028ae5e6648b82
ecourse <smkang@codenuri.co.kr>
2025-04-17 14:47:09 +0900

    Add files via upload

--- DAY4/5_람다_지역변수캡쳐2.cpp
+++ DAY4/5_람다_지역변수캡쳐2.cpp
@@ -5,9 +5,16 @@ int main()
 {
 	int v1 = 10, v2 = 10;
 
-	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+	// 람다 표현식 : () 연산자가 상수 멤버 함수 입니다.
+	// mutable 람다 표현식  : () 연산자가 상수 멤버 함수가 아닙니다.
+	auto f1 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; }; 
+
+	f1(0); // v1 = 100 실행됨. 하지만 람다표현식객체안의 복사본이 변경된것
+
+	std::cout << v1 << std::endl; // 10
 
 
+	/*
 	// 지역변수 캡쳐의 원리
 	class CompilerGeneratedName
 	{
@@ -17,7 +24,8 @@ int main()
 	public:
 		CompilerGeneratedName(int a, int b) : v1(a), v2(b) {}
 
-		inline auto operator()(int a) const 
+//		inline auto operator()(int a) const 
+		inline auto operator()(int a)			// mutable 람다 표현식
 		{
 			v1 = 100;
 			return a + v1 + v2;
@@ -25,6 +33,7 @@ int main()
 	};
 	auto f1 = CompilerGeneratedName{ v1, v2 }; // main 지역변수를 생성자인자로전달
 
+	*/
 
 
 }
