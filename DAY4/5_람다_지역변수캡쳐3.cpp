5e2ac466ead71abbb528780098a9c8d66da787a3
ecourse <smkang@codenuri.co.kr>
2025-04-17 14:56:07 +0900

    Add files via upload

--- DAY4/5_람다_지역변수캡쳐3.cpp
+++ DAY4/5_람다_지역변수캡쳐3.cpp
@@ -8,7 +8,7 @@ int main()
 
 //	auto f1 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; };
 
-	auto f1 = [&v1, &v2](int a) mutable { v1 = 100; return a + v1 + v2; };
+	auto f1 = [&v1, &v2](int a) { v1 = 100; return a + v1 + v2; };
 
 	f1(0);  // main 함수의 원본 v1 변경
 
@@ -26,14 +26,17 @@ int main()
 	public:
 		CompilerGeneratedName(int& a, int& b) : v1(a), v2(b) {}
 
-//		inline auto operator()(int a) const
-		inline auto operator()(int a)			// mutable 람다 표현식
+		inline auto operator()(int a) const
 		{
-			v1 = 100;
+			v1 = 100;	// reference 는 결국 포인터
+						// 이 코드는 v1 자체를 변경하는 것이 아니라
+						// v1 이 가리키는 곳을 변경한것
+						// 따라서 "상수 멤버 함수" 안에서도 사용가능. 
+
 			return a + v1 + v2;
 		}
 	};
-	auto f1 = CompilerGeneratedName{ v1, v2 }; // main 지역변수를 생성자인자로전달
+	auto f1 = CompilerGeneratedName{ v1, v2 }; 
 
 	*/
 
