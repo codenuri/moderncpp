25c6ca02501048d08f73d05f31eb0e49fa0c8751
ecourse <smkang@codenuri.co.kr>
2025-04-17 16:47:44 +0900

    Add files via upload

---
+++ DAY4/QA1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Dialog
+{
+public:
+	void foo() { std::cout << "foo\n"; }
+};
+
+int main()
+{
+	Dialog d;
+
+	d.foo();			// lvalue 로 멤버 함수 호출
+	Dialog{}.foo();		// rvalue 로 멤버 함수 호출
+}
