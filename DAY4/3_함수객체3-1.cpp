12aabfa2b7871f22581882640f4e0d843687aab3
ecourse <smkang@codenuri.co.kr>
2025-04-17 10:39:05 +0900

    Add files via upload

--- DAY4/3_함수객체3-1.cpp
+++ DAY4/3_함수객체3-1.cpp
@@ -8,7 +8,6 @@ bool foo(int n) { return n % 3 == 0; }
 struct IsDivide
 {
 	int value;
-	
 	IsDivide(int n) : value(n) {}
 
 	bool operator()(int n)  { return n % value == 0; }
@@ -20,10 +19,15 @@ int main()
 
 	int k = 3;
 
-	IsDivide f(k); // f 는 내부적으로 k 값을 캡쳐(보관) 합니다.
-	int ret = f(10); // ?
+	IsDivide f(k);	// f 는 내부적으로 k 값을 캡쳐(보관) 합니다.
+					// f 는 단항함수 처럼 사용 가능합니다.
 
-	auto p2 = std::find_if(v.begin(), v.end(), ? );
+	bool ret = f(10); // "10 % k(3) == 0" 을 조사하는 함수
+
+
+	auto p2 = std::find_if(v.begin(), v.end(), f );
+
+	std::cout << *p2 << std::endl;// 6
 }
 
 
