3080b6d7e12a81e1f17fe35918574e1a3577b063
ecourse <smkang@codenuri.co.kr>
2025-04-17 16:36:21 +0900

    Add files via upload

--- DAY4/6_람다기타4.cpp
+++ DAY4/6_람다기타4.cpp
@@ -5,6 +5,25 @@ class CompilerGeneratedName
 {
 public:
 	inline int operator()(int a, int b) const { return a + b; }
+
+	// 함수 포인터로의 변환을 위해서
+	// operator() 와 동일한 일을 하는 static 멤버 함수가 추가됩니다.
+	static int helper(int a, int b)
+	{
+		return a + b;
+	}
+	
+	using FP = int(*)(int, int);
+	
+	// 변환 연산자 함수 : 객체가 다른 타입으로 변환될수 있게 할때 사용
+	// => 함수 이름에 리턴 타입이 있기 때문에 별도로 반환 타입표기 안합니다.
+	// => ex) operator int(), operator double()
+
+//	operator FP() { return &CompilerGeneratedName::operator(); }
+						// => error
+						// => operator() 는 멤버 함수이므로 this 추가됨
+
+	operator FP() { return &CompilerGeneratedName::helper; }
 };
 
 int main()
@@ -12,6 +31,10 @@ int main()
 //	int(*f)(int, int) = [](int a, int b) {return a + b; };
 
 	int(*f)(int, int) = CompilerGeneratedName{};
+					// 객체.operator int() => 객체가 int로 변환가능
+					// 객체.operator 함수포인터타입() 이 필요
+
+	int n = f(1, 2);
 }
 
 
