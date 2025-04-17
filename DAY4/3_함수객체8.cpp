e0334f6f3daffd92cb144e5880184e3de2ce0e12
ecourse <smkang@codenuri.co.kr>
2025-04-17 12:47:09 +0900

    Add files via upload

--- DAY4/3_함수객체8.cpp
+++ DAY4/3_함수객체8.cpp
@@ -47,6 +47,25 @@ int main()
 {
 	init();
 
-	chronometry(ex1);
+//	chronometry(ex1);
 	chronometry(ex2);
+	chronometry(ex1);
 }
+
+// 참고 : 함수 타입과 함수 포인터 타입 을 잘 구별해 두세요
+// => 처음 보시는 분은 "C" 언어 에서 찾아 보세요
+
+//int cmp(int a, int b) 에서
+
+// 함수 타입        : int(int, int)
+// 함수 포인터 타입 : int(*)(int, int)
+
+// C/C++ : signature 가 동일한 함수는 같은 "타입" 이고
+//         따라서, 같은 "함수포인터 타입" 이다.
+// 
+// Rust : 모든 함수는 "다른 타입" 이다.
+//        signature 가 동일한 함수도 다른 타입이다.
+//        하지만 signature 가 동일한 함수는 같은 함수 포인터 타입에 담을수 있다.
+// 
+// 
+//		
