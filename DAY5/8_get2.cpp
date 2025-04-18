47cabb7e3bbc12f0120520276d475c4a969346b7
ecourse <smkang@codenuri.co.kr>
2025-04-18 15:05:25 +0900

    Add files via upload

---
+++ DAY5/8_get2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+template<typename ... Types>
+struct tuple
+{
+	static constexpr int N = 0;
+};
+template<typename T, typename ... Types>
+struct tuple<T, Types...> : public tuple<Types...>
+{
+	using base = tuple<Types...>;
+
+	T value;
+	tuple() = default;
+	tuple(const T& v, const Types& ... args)
+		: value(v), base(args...) {
+	}
+
+	static constexpr int N = base::N + 1;
+};
+
+int main()
+{
+	tuple<int, double, short> t(1, 3.4, 3);
+
+	std::cout << t.value << std::endl; // ??
+
+
+	std::cout << ?   << std::endl; // 3.4
+
+	std::cout << ? << std::endl; // 3
+
+
+}
