cce62e0531cc4ea8681e2ab6c44e3ea714785b70
ecourse <smkang@codenuri.co.kr>
2025-04-18 10:35:31 +0900

    Add files via upload

--- DAY5/2_type_traits7.cpp
+++ DAY5/2_type_traits7.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <type_traits>
 
+
+// 인스턴스화(instantiation)
+// => 템플릿 => 실제 C++ 함수(클래스)를 만드는 과정
+
+// if : 실행시간 조건문
+// => 조건이 false 라면 인스턴스화에 포함된다.
+
+// if constexpr : 컴파일시간 조건문
+// => 조건이 false 라면 인스턴스화에 포함되지 않는다.
+
 template<typename T> void printv(const T& v)
 {
-	if ( std::is_pointer_v<T> )
+	
+
+
+//	if ( std::is_pointer_v<T> )
+	if constexpr (std::is_pointer_v<T>)
+
 		std::cout << v << " : " << *v << std::endl;
 	else
 		std::cout << v << std::endl;
