69965f1aa98c039d38134df0ae1ae7c86c609142
ecourse <smkang@codenuri.co.kr>
2025-04-18 15:25:37 +0900

    Add files via upload

--- DAY5/8_get3.cpp
+++ DAY5/8_get3.cpp
@@ -20,13 +20,53 @@ struct tuple<T, Types...> : public tuple<Types...>
 };
 //-----------------------------------------
 
+// primary template
+template<int N, typename TP>
+struct tuple_element
+{
+	// TP 에서 N 번째 요소 타입을 구해서 ? 채워야 합니다.
+//	using type = ? 
+	
+	// 현재는 구할수 없습니다.
+	// 타입을 구할수 있도록 부분 특수화 해야 합니다
+};
+
+// N == 0 으로 부분 특수화
+/*
+template<typename TP>
+struct tuple_element<0, TP>
+{
+	// TP 에서 0 번째 요소 타입을 구하면 됩니다.
+	// => 하지만 구할수 없습니다. 잘못된 부분특수화 입니다.
+	using type = ? ;
+
+};
+*/
+/*
+template<typename ... Types>
+struct tuple_element<0, tuple<Types...> >
+{
+	// 역시 "tuple<Types...>" 에서 0 번째 타입을 구할수 없습니다.
+	// => 잘못된 부분 특수화
+	using type = ? ;
+};
+*/
+
+template<typename T, typename ... Types>
+struct tuple_element<0, tuple<T, Types...> >
+{
+	using type = T ;
+};
+
 template<typename TP>
 void test(TP& tp)
 {
 	// 현재 TP : tuple<int, double, short> 입니다.
 
 	typename tuple_element<0, TP>::type n1; // int n1 되야 합니다.
-	typename tuple_element<1, TP>::type d1; // double d1 되야 합니다.
+//	typename tuple_element<1, TP>::type d1; // double d1 되야 합니다.
+
+	std::cout << typeid(n1).name() << std::endl;
 }
 
 int main()
