0d4a2104a34f75af046e72acef368971ee8267fe
ecourse <smkang@codenuri.co.kr>
2025-04-18 09:36:09 +0900

    Add files via upload

--- DAY5/2_type_traits3.cpp
+++ DAY5/2_type_traits3.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <type_traits>
 
+// 변수 선언문에서 변수 이름을 제거 하면 "타입" 이 남게 됩니다.
+/*
+int    n;	// 변수이름 : n		타입 : int
+double d;	// 변수이름 : d      타입 : double
+void foo(int); // 함수이름 : foo	  타입 :void(int)
+int x[3];	// 변수 이름 : x		타입 : int[3]
+		
+int[3]; // int     3개 크기 배열
+T[3];   // 임의타입 3개 크기 배열
+T[N];   // 임의타입 모든 크기 배열
+
+//T[];// 크기를 알수 없는 배열(unknown size array) 
+	  // C에서 특별한 경우 함수인자등에서 사용 ( C 매뉴얼 참고)
+*/
+
+
+
 template<typename T> struct is_array
 {
 	static constexpr bool value = false;
 };
 
-template<typename T> struct is_array< T[] >
+template<typename T, int N> struct is_array< T[N] >
 {
 	static constexpr bool value = true;
 };
