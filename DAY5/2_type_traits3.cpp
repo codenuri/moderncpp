4b348fb4c437156a378ec081cf3728b6d6d8be1c
ecourse <smkang@codenuri.co.kr>
2025-04-18 09:38:36 +0900

    Add files via upload

--- DAY5/2_type_traits3.cpp
+++ DAY5/2_type_traits3.cpp
@@ -21,19 +21,21 @@ T[N];   // 임의타입 모든 크기 배열
 template<typename T> struct is_array
 {
 	static constexpr bool value = false;
+	static constexpr int size = -1;
 };
 
 template<typename T, int N> struct is_array< T[N] >
 {
 	static constexpr bool value = true;
+	static constexpr int size = N ;
 };
 
 
 template<typename T> void foo(T& a)
 {
-	// is_array 만들어 보세요
+	// T : int[3]
 	if ( is_array<T>::value ) 
-		std::cout << "배열" << std::endl;
+		std::cout << "배열, 크기는 " << is_array<T>::size  << std::endl;
 	else
 		std::cout << "배열 아님" << std::endl;
 }
