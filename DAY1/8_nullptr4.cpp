88fc4d010ba1b8677aa48711bb8feca1e927abb8
ecourse <smkang@codenuri.co.kr>
2025-12-08 13:45:13 +0900

    Add files via upload

--- DAY1/8_nullptr4.cpp
+++ DAY1/8_nullptr4.cpp
@@ -3,17 +3,24 @@
 
 void foo(int* p) {}
 
+template<typename F, typename ARG>
+void forward_parameter(F f, ARG arg) // int arg = 0;	foo(arg) 이 에러
+{									 // std::nullptr_t arg = nullptr; foo(arg) 은 ok
+	f(arg);
+}
+
 int main()
 {
-	forward_parameter(&foo, 0); 
+	// foo 의 인자는 포인터
+	// 0 을 사용하면
+	foo(0); // 직접 전달은 ok
+	
+	forward_parameter(&foo, 0);  // error. 하지만 간접 전달은 모두 에러!
+
 	forward_parameter(&foo, nullptr);
 }
 
-template<typename F, typename ARG>
-void forward_parameter(F f, ARG arg) 
-{
-	f(arg); 
-}
+
 
 
 
