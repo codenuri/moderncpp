46d1a5462b43fcd7e69eec973db114b86a64062f
ecourse <smkang@codenuri.co.kr>
2025-12-08 13:06:04 +0900

    Add files via upload

---
+++ DAY1/6_위임생성자.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// 6_위임생성자 - 24page
+
+class Point
+{
+public:
+	int x, y;
+public:
+	Point() : Point(0, 0)  // 이 코드가 핵심. 하나의 생성자에서 다른 생성자를 호출하는 기술
+	{						// C++11 delegate constructor 라는 문법
+
+		// 여기서 다른 생성자를 호출할수 없을까 ?
+		// => 여기서 다른 생성자 호출할수 없습니다.
+	//	Point(0, 0); // 컴파일 에러는 아님
+						// 이 코드는 "임시객체(temporary)" 를 만드는 코드(내일 배우는 주제)
+	}
+	Point(int a, int b) : x(a), y(b)
+	{
+		// 이 안에 복잡한 초기화 관련 코드가 있다. 
+	}
+};
+int main()
+{
+	Point p;
+	std::cout << p.x << std::endl;
+
+}
+
