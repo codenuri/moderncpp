5e882e5685746d40add3c7d993035f2c5eb9e4d6
ecourse <smkang@codenuri.co.kr>
2025-12-08 14:11:15 +0900

    Add files via upload

--- DAY1/9_enum2.cpp
+++ DAY1/9_enum2.cpp
@@ -10,6 +10,11 @@ int main()
 	COLOR c1 = COLOR::red;	// ok
 	int   c2 = COLOR::red;  // error. int 타입으로 형변환 안됨
 	COLOR c3 = red;			// error. COLOR 이 반드시 필요
+
+	// C기법중 enum 값 끼리 - 하는 기법도 있습니다.
+	// => 명시적 cast 하면 int 로 변경가능합니다.
+	int n1 = static_cast<int>(c1); // ok.. 1
+	int n2 = static_cast<int>(COLOR::blue); // ok..3
 }
 
 
