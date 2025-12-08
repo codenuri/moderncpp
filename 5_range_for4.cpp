6146dfde72744857d35bff4f7a18297ebf355542
ecourse <smkang@codenuri.co.kr>
2025-12-08 11:15:32 +0900

    Add files via upload

---
+++ 5_range_for4.cpp
@@ -0,0 +1,30 @@
+// 5_range_for4.cpp
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	for (auto& n : v)
+	{
+		std::cout << n << std::endl;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
