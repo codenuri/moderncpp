3eca47d00dda33c2fc4ad6d75da8a0178f3914c7
ecourse <smkang@codenuri.co.kr>
2025-12-09 08:57:14 +0900

    Add files via upload

--- DAY2/1_structure_binding2.cpp
+++ DAY2/1_structure_binding2.cpp
@@ -1,6 +1,8 @@
 // 19_structure_binding - 52 page
 #include <map>
 #include <string>
+#include <vector>
+#include <ranges>
 
 int main()
 {
@@ -17,5 +19,28 @@ int main()
 	p.second = "화요일";
 	m.insert(p);
 
+	// C++17 이전에 map 의 모든 요소 열거
+	for (auto p : m)
+	{
+		// p는 pair 입니다.
+		auto key = p.first;
+		auto value = p.second;
+	}
+
+	// C++17 이후
+	for (auto [key, value] : m)
+	{
+	}
+
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	for (auto n : v) // 요소만 열거. 
+	{
+	}
+
+	// 아래 코드는 C++23
+	for (auto [idx, value] : std::ranges::enumerate_view(v)) // index, 요소 를 tuple로
+	{
+	}
 }
 
