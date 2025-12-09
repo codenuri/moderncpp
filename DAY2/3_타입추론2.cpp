83ba26693754b542d818e02c203bca51c0589678
ecourse <smkang@codenuri.co.kr>
2025-12-09 11:15:03 +0900

    Add files via upload

--- DAY2/3_타입추론2.cpp
+++ DAY2/3_타입추론2.cpp
@@ -11,18 +11,16 @@ template<typename T> void foo(T a)
 	// __PRETTY_FUNCTION__ : g++, clang++ 에서만 가능. vs 안됨
 	// 
 	// std::cout << __func__ << std::endl;
-	// std::cout << __FUNCSIG__ << std::endl;
+	std::cout << __FUNCSIG__ << std::endl;
 
 	// #2. godbolt.org 에서 확인 - 가장 권장
 
 	// #3. RTTI 기술 사용
 	// => 정확한 타입 출력 못함. 사용하지 마세요
+	// => 타입이 가진, const, reference 를 출력하지 못합니다.
 	std::cout << typeid(T).name() << std::endl;
 
 }
-
-
-
 
 
 
@@ -32,7 +30,8 @@ int main()
 	// #1. 타입 인자를 명시적으로 전달하는 경우
 	// => 무조건 사용자가 전달한 타입 사용
 	// => 컴파일러가 타입 추론 안함
-	foo<int>(3.4);
+//	foo<int>(3.4);
+	foo<const int>(3.4);
 
 	// #2. 타입 인자를 전달하지 않는 경우.
 	// => 컴파일러가 함수 인자를 보고 타입을 추론
