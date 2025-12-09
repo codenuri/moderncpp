a83316070fa64d0d6f0c54a30323efb9929460b6
ecourse <smkang@codenuri.co.kr>
2025-12-09 16:58:27 +0900

    Add files via upload

--- DAY2/5_임시객체8.cpp
+++ DAY2/5_임시객체8.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 
+template<typename T>
 class Vector
 {
-	int* ptr;
+	T* ptr;
 public:
 	Vector(int sz)
 	{
-		ptr = new int[sz];
+		ptr = new T[sz];
 	}
 	~Vector() { delete[] ptr;}
 	
-	int at(int idx) { return ptr[idx]; }
+	// 아래처럼 하면 v.at(0) =  0; 안됨
+//	T at(int idx) { return ptr[idx]; }
+
+	// 아래 처럼하면 ok. 
+	T& at(int idx) { return ptr[idx]; }
+
+	T& operator[](int idx) { return ptr[idx]; }
 };
 
 int main()
 {
-	Vector v(5); // 5개 크기 vector 
+	Vector<int> v(5); // 5개 크기 vector 
 
-	v.at(3) = 0;  
+	v.at(2) = 0;  
+	v.at(3) = 0; 
+	v[2] = 0;
+
+	std::cout<< v.at(2) << std::endl; // 0
 }
