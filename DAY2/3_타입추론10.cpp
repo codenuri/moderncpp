7ac57f81288d77d40824b218d3643e148dda9d39
ecourse <smkang@codenuri.co.kr>
2025-12-09 15:00:25 +0900

    Add files via upload

---
+++ DAY2/3_타입추론10.cpp
@@ -0,0 +1,14 @@
+﻿
+// auto 와 decltype 추론 차이
+
+int main()
+{
+	int x[3] = { 1,2,3 };
+
+	auto a = x[0];		// int
+	decltype(x[0]) d; 	// int&
+
+	// decltype : id 외에 다른 연산자가 있다면
+	// => lvalue 인지 rvalue 인지까지 고려해서 타입결정
+}
+
