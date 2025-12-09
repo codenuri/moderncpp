f3be3f17a95b2a7b4a6044eb2d71de886d7be07e
ecourse <smkang@codenuri.co.kr>
2025-12-09 16:12:45 +0900

    Add files via upload

--- DAY2/5_임시객체4.cpp
+++ DAY2/5_임시객체4.cpp
@@ -23,12 +23,34 @@ int main()
 	Point* p1 = &pt;			// ok
 	Point* p2 = &Point{ 1, 2 };	// error
 							
-	// #3. temporary 는 reference 로 가리킬수 없다.
+	// #3. temporary 는 non-reference 로 가리킬수 없다.
 	Point& r1 = pt;				// ok
 	Point& r2 = Point{ 1, 2 };	// error
 
 	// #4. temporary 는 const reference 로는 가리킬수 있다.
+	// => draw_line( Point{0, 0}, Point{5,5}) 가 되게 하려면 아래 문법 필요
+	// => init( Point{0,0}) 이 안되게 하려면 #3 문법 필요
+
 	const Point& r3 = Point{ 1, 2 };	// ok
+					  // => 이순간 temporary 의 수명은 r3 의 수명으로 연장됩니다.
+					  // => life-time extension 이라는 불리는 개념
+					  // => Rust 등의 다른 언어도 가지고 있는 개념
+
+	int x = r3.x; // ok
+	r3.x = 10; // error. const.!!
+
+	// 여기까지 설명은 C++98 시절 이야기 
+
+	// #5. C++11 만들때 "temporary" 를 const 없이 참조로 가리킬 필요가 생겼다
+	// => move 와 perfect forwarding 을 만들기 위해
+	Point&& r4 = Point{1, 2}; // rvalue 만 가리키는 전용 reference
+							  // 수명 연장됨
+	Point&& r5 = pt; // error. Point&& 는 rvalue(temporary) 만 가리킬수 있다.
+					// 이름있는 객체(lvalue) 는 가리킬수 없다.
+	// 이제 용어가 아래 처럼 분류 됩니다.
+	// Point&& : rvalue reference
+	// Point&  : lvalue reference
+
 }
 
 
