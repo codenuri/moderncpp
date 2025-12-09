92961f75daae85103270f11a6a5caba9c6f6d809
ecourse <smkang@codenuri.co.kr>
2025-12-09 10:29:01 +0900

    Add files via upload

---
+++ DAY2/2_초기화10.cpp
@@ -0,0 +1,33 @@
+﻿// 
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v1 = { 1,2,3,4,5 };
+	std::vector<int> v2{ 1,2,3,4,5 };
+
+
+	// #1. 아래 2줄의 차이점은 ?
+	std::vector<int> v3(10, 2); // vector(int, int) 생성자
+								// => 10개의 요소를 2로 초기화
+								// => v3.size() : 10
+
+	std::vector<int> v4{10, 2}; // vector(std::initializer_list) 생성자
+								// => 2개를 10, 2 로 초기화
+								// => v4.size() : 2
+
+
+	// #2. 아래 코드에서 에러는 ?
+	std::vector<int> v5 = 10;	// error
+								// explicit vector(int) 이므로!!
+
+	std::vector<int> v6 = {10}; // ok
+								// vector(std::initializer_list) 생성자는
+								// explicit 가 아님.
+								
+}
+
+
+
+
---
+++ DAY2/2_초기화9.cpp
@@ -0,0 +1,46 @@
+﻿// 63 page
+#include <iostream>
+#include <vector>
+
+// std::initializer_list<int>  와 생성자
+// => std::initializer_list<int> 를 만든 주된 이유는 "생성자"에서 사용하려고!
+
+class Point
+{
+	int x, y;
+public:
+	Point(int)      { std::cout << "int" << std::endl;	}
+	Point(int, int) { std::cout << "int, int" << std::endl; }
+	Point(std::initializer_list<int> e) { std::cout << "initializer_list<int>" << std::endl; }
+};
+int main()
+{
+	// 아래 코드가 어떤 생성자를 사용할지 생각해 보세요
+	Point p1(1);		// Point(int)
+	Point p2(1, 2);		// Point(int, int)
+	Point p3({ 1,2 });	// Point(std::initializer_list<int>)
+
+	Point p4{ 1,2 };	// 핵심
+						// 1. Point(std::initializer_list<int>) 있다면 호출
+						// 2. (1)이 없다면 Point(int, int)
+
+	Point p5(1, 2, 3);  // error. Point(int, int, int) 생성자는 없다
+
+	Point p6{ 1, 2, 3 }; // ok. Point(std::initializer_list<int>) 사용
+
+	Point p7 = { 1,2,3 }; // ok. 
+						// Point(std::initializer_list<int>)는 explicit 아님
+
+	// 핵심 
+	// => 생성자 인자로 std::initializer_list 를 사용하면
+	// => {} 초기화시에 "인자의 갯수를 가변"으로 사용할수 있습니다.
+
+	// C++11 만들때 STL 을 아래 처럼 사용하게 하고 싶었습니다.
+	// => 이때, 근간이 되는 어떤 문법이 필요 했습니다.
+	// => 해결책이 std::initializer_list 라는 존재이고
+	// => 생성자에서만 특별하게 동작(위 p4 규칙) 하는 문법을 만든것
+	std::vector<int> v1 = { 1,2,3,4,5 };
+	std::vector<int> v2{ 1,2,3,4,5 };
+}
+
+
