0a2d663a82e1bf5178f1697b018005b0cbba2a4b
ecourse <smkang@codenuri.co.kr>
2025-12-10 08:34:21 +0900

    Add files via upload

---
+++ DAY3/1_rvalue1.cpp
@@ -0,0 +1,22 @@
+﻿// 1_rvalue - 87 page
+
+int main()
+{
+	int v1 = 10;
+
+	v1 = 20;	// ok    v1 은 = 의 왼쪽에 올수 있다. lvalue
+	10 = v1;	// error 10 은 = 의 왼쪽에 올수 없고, 오른쪽에만 가능 rvalue
+	
+	int n = v1; // ok. v1 은 오른쪽도 가능.
+}
+
+// lvalue : 등호의 양쪽에 올수 있다.
+// rvalue : 등호의 오른쪽만 가능, 왼쪽 안됨
+
+
+
+
+
+
+
+
