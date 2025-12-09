3c2c93f8d6ca9b47a2900115d7b199427e564f6c
ecourse <smkang@codenuri.co.kr>
2025-12-09 16:36:40 +0900

    Add files via upload

--- DAY2/5_임시객체6.cpp
+++ DAY2/5_임시객체6.cpp
@@ -10,12 +10,26 @@ public:
 		++cnt;
 		return *this;
 	}
+
+	Counter() = default; // 디폴트 생성자
+
+	Counter( const Counter& c) : cnt{c.cnt}
+	{
+		std::cout << "복사생성자 호출됨. 생성된 객체 cnt = " << cnt << std::endl;
+	}
+
+	~Counter() 
+	{
+		std::cout << "Counter 파괴 : cnt = " << cnt << std::endl;
+	}
 };
 
 int main()
 {
 	Counter c;
+	std::cout << "---------------------------------\n";
 	c.increment().increment().increment();
+	std::cout << "---------------------------------\n";
 
 	std::cout << c.cnt << std::endl; // 결과 예측해 보세요
 }
