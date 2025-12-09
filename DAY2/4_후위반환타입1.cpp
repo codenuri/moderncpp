946da1e23732751c9b8db89bf136d58a3ab7a33b
ecourse <smkang@codenuri.co.kr>
2025-12-09 15:04:31 +0900

    Add files via upload

---
+++ DAY2/4_후위반환타입1.cpp
@@ -0,0 +1,20 @@
+﻿// 후위 반환 타입 -  73 page
+
+// C/C++의 일반적인 함수 모양
+int square(int a)
+{
+	return a * a;
+}
+
+// C++11 에서 추가된 새로운 함수 모양
+// => suffix return type 이라는 문법	
+// => 파이썬, Rust, Swift 등의 언어가 사용하는 방식
+auto square2(int a) -> int 
+{
+	return a * a;
+}
+
+int main()
+{
+	square(3);
+}
