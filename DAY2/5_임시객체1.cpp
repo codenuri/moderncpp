b72333901a8452303a7aefb3a389725378353594
ecourse <smkang@codenuri.co.kr>
2025-12-09 15:36:28 +0900

    Add files via upload

--- DAY2/5_임시객체1.cpp
+++ DAY2/5_임시객체1.cpp
@@ -14,10 +14,13 @@ int main()
 //	Point pt(1, 2); // 이름이 있는 객체(pt 가 이름), named object
 					// 수명 : 자신을 선언한 {} 을 벗어 날때 파괴
 
-//	Point (1, 2);	// unnamed object. 
+//	Point {1, 2};	// unnamed object. 
 					// 수명 : 자신을 선언한 문장의 끝(; 만날때 파괴)
-
-	Point(1,2), std::cout << "X\n";
+					// 흔히 "temporary, 임시객체" 라고 합니다.
+					// 이때 Point(1,2) 하면 함수 호출 처럼 보일수 있습니다.
+					// Point{1,2} 가 좀더 가독성이 좋습니다.
+					
+	Point{1,2}, std::cout << "X\n";
 
 	std::cout << "-------" << std::endl;
 }
