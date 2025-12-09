6a4cb334e98db03e184d92569ea7d3b7874c399f
ecourse <smkang@codenuri.co.kr>
2025-12-09 11:04:03 +0900

    Add files via upload

--- DAY2/3_타입추론1.cpp
+++ DAY2/3_타입추론1.cpp
@@ -17,4 +17,14 @@ int main()
 	// template : 함수 인자를 보고 타입을 결정
 	foo(3); // T = int 로 결정
 
+	// type deduction(추론)
+	// => 컴파일러가 문맥을 보고 타입을 결정하는것
+	// => 다른 언어는 "type inference" 라는 용어를 사용
+
+	// C++ 은 3가지 경우에 추론 발생
+	// 1. template
+	// 2. auto
+	// 3. decltype
+	// => 1,2 는 동일한 타입 추론 규칙 사용. template 으로 먼저 학습하는것이 쉽다.
+	// => 3은 다른 규칙.
 }
