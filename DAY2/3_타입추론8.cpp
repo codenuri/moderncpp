8290247d7b2d6900454ec1b22c7ea3ed560389d4
ecourse <smkang@codenuri.co.kr>
2025-12-09 14:32:36 +0900

    Add files via upload

---
+++ DAY2/3_타입추론8.cpp
@@ -0,0 +1,24 @@
+﻿// decltype 추론 - 71page
+int main()
+{
+	int n = 10;
+	int& r = n;
+	const int c = 10;
+	int* p = &n;
+	
+	// auto : 우변의 표현식으로 타입 추론
+	auto a = r; // 
+	
+	// decltype : () 안의 표현식으로 타입 추론	
+	// => 언제 사용하나요 ? => 주로 template 만들때 사용(뒤에 예제 참고)
+	
+	// 추론 규칙 #1. () 안에 변수 이름만 있을때
+	// => 변수의 선언을 보고 타입 결정
+	decltype(n) d1; // int d1
+	decltype(r) d2; // int& d2  인데, 초기값 없으므로 error
+	decltype(c) d3; // const int d3 인데, 초기값 없으므로 error
+	decltype(p) d4; // int* d4
+}
+
+
+
