618176acacde8d38f3a15149eaef8cda81a6c73a
ecourse <smkang@codenuri.co.kr>
2025-12-10 15:22:02 +0900

    Add files via upload

--- DAY3/4_forwarding_reference3.cpp
+++ DAY3/4_forwarding_reference3.cpp
@@ -18,11 +18,25 @@ template<typename T> void f3(T& a)
 
 int main()
 {
+	int n = 3;
+
 	// 타입을 직접 전달하는 경우
-	f3<int>(? );		// T = ?     T& = ?		f3(?)
-	f3<int&>(? );
-	f3<int&&>(? );
+	f3<int>(n );		// T = int    T& = int&		f3(int&)
+	f3<int&>(n );		// T = int&   T& = int& &	f3(int&)
+	f3<int&&>(n );		// T = int&&  T& = int&& &	f3(int&)
+
+	// 타입을 전달하지 않으면 컴파일러가 함수 인자로 타입 결정
+	// => 최대한 함수를 생성하려고 노력합니다.
+	f3(n); // T = int		T& = int&		f3(int&) ok.. 
+	f3(3); // T = int		T& = int&		f3(int&) 인데 f3(3) 이므로 error
+			// 혹시, T 를 int 가 아닌
+			// int&, int&& 로 결정하면 받을수 있을까 ? 안된다..
 }
+
+
+
+
+
 
 
 
