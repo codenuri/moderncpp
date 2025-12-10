ece4c2dbb94ca4e35e157c8b7a68ec49468e5fec
ecourse <smkang@codenuri.co.kr>
2025-12-10 13:23:31 +0900

    Add files via upload

--- DAY3/3_move08.cpp
+++ DAY3/3_move08.cpp
@@ -3,11 +3,19 @@
 
 // 자동 생성 규칙.
 
+// 규칙 #1. 사용자가 복사 계열과 이동 계열(이동생성자, 이동 대입연산자) 를
+//          모두 만들지 않으면
+// => 컴파일러가 모두 제공해 준다
+// => 컴파일러 제공 복사 생성자 : 모든 멤버 데이타를 "복사" 
+// => 컴파일러 제공 이동 생성자 : 모든 멤버 데이타를 "이동"
+
 struct Object
 {
 	std::string name;
 
-	Object(const std::string& n) : name(n) {}
+	Object(const std::string& n) : name(n) {} // 일반 생성자
+
+
 };
 
 int main()
@@ -16,8 +24,8 @@ int main()
 	Object o2("obj2");
 
 	Object o3 = o1;
-	Object o4 = std::move(o1);
+	Object o4 = std::move(o2);
 
-	std::cout << o1.name << std::endl;
-	std::cout << o2.name << std::endl;
+	std::cout << o1.name << std::endl; // "obj1"
+	std::cout << o2.name << std::endl; // ""
 }
