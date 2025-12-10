edcde08255f7b0621d38eb72f362cfb206a5ba83
ecourse <smkang@codenuri.co.kr>
2025-12-10 13:52:20 +0900

    Add files via upload

---
+++ DAY3/3_move09.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+// rule of 0  => 권장
+
+// 자원을 직접 관리하지 말고, 자원 관리 클래스에 의존하라!
+
+// 문자열이 필요 하면 : std::string
+// 동적배열이 필요    : 스마트 포인터 또는 std::vector
+
+// 자원을 직접 관리하지 않으면
+// => 소멸자, 복사 생성자, 대입연산자, 이동생성자, 이동대입 모두 필요 없다!!
+// => 하지만 컴파일러가 모두 완벽하게 제공..
+// => 가장 잘만든 코드. 
+
+class Cat
+{
+	std::string name;
+	int   age;
+	std::string address;
+
+public:
+	Cat(const std::string& n, int a, const std::string& addr = "unknown")
+		: name{ n }, age {a}, address{ addr }
+	{
+	}
+};
+
+int main()
+{
+	Cat c1("nabi", 2);
+
+	Cat c2 = c1;
+	c2 = c1;
+
+	Cat c3 = std::move(c1);
+	c3 = std::move(c2);
+
+}
+
+
+
+
---
+++ DAY3/3_move10.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+class Object
+{
+public:
+	Object() {}
+
+	// 복사 생성자
+	Object(const Object&) {	std::cout << "copy" << std::endl;	}
+	
+	// 이동 생성자 
+	// 이동 생성자 구현시
+	// 1. 되도록 예외가 없도록 만드세요(대부분 예외 없이 구현가능, 포인터복사만하므로)
+	// 2. 그리고, 예외가 없음을 컴파일러에게 알려 주세요(noexcept)
+	Object(Object&&) noexcept
+	{
+		std::cout << "move" << std::endl;
+	}
+};
+
+int main()
+{
+	Object o1;
+	Object o2 = o1; // 항상 복사 생성자 사용
+	Object o3 = std::move(o1); // 항상 이동 생성자 사용
+	Object o4 = std::move_if_noexcept(o2); // 이동 생성자가 예외가 없으면 사용
+										   // 예외가능성이 있으면 복사 생성자 사용
+										   // vector 가 내부적으로 버퍼 복사시
+											// std::move() 가 아니라
+											// std::move_if_noexcept() 사용
+
+
+
+	std::vector<Object> v(5);
+
+	std::cout << "----------------" << std::endl;
+
+	v.resize(10); // 이순간의 원리를 생각해봅시다.
+
+	std::cout << "----------------" << std::endl;
+}
+
