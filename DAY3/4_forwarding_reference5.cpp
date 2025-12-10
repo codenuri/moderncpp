d5ca00f79a5b44665417595d4f0aa62440bee51b
ecourse <smkang@codenuri.co.kr>
2025-12-10 15:47:59 +0900

    Add files via upload

---
+++ DAY3/4_forwarding_reference5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+template<typename T> void foo(T&& arg)
+{
+	// 확인하는법
+	std::cout << __FUNCSIG__ << std::endl;
+}
+int main()
+{
+	int n = 10;
+
+	foo(n);  // T = int&, T&& = int& &&,  foo(int& arg)
+	foo(10); // T = int,  T&& = int&&,    foo(int&& arg)
+}
+
+
+
+
