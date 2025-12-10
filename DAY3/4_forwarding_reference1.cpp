2cf8d96dbabe805184e999542f5e485f7ae27fa9
ecourse <smkang@codenuri.co.kr>
2025-12-10 14:44:35 +0900

    Add files via upload

---
+++ DAY3/4_forwarding_reference1.cpp
@@ -0,0 +1,14 @@
+﻿// forwarding reference 란 ?
+// => "T&&" 템플릿
+
+// int&& : rvalue reference
+// T&&   : forwarding reference
+
+template<typename T> void chronomety(T&& a)
+{
+}
+
+int main()
+{
+
+}
