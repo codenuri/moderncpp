4cade4dd630b1296ded67072c04a17de2b8c30c5
ecourse <smkang@codenuri.co.kr>
2025-12-10 13:13:17 +0900

    Add files via upload

---
+++ DAY3/3_move07.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Cat
+{
+	char* name;
+	int   age;
+	std::string address;
+
+public:
+	Cat(const char* n, int a, const std::string& addr = "unknown") 
+		: age{ a }, address{ addr }
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+	}
+	~Cat() { delete[] name; }
+
+	Cat(const Cat& c) : age{ c.age }, address{addr}
+	{
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+	}
+
+	Cat& operator=(const Cat& c)
+	{
+		if (&c == this)
+			return *this;
+
+		age = c.age;
+		address = c.address;
+		delete[] name;
+
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+
+		return *this;
+	}
+	Cat(Cat&& c) : name{ c.name }, age{ c.age }, address{ addr }
+	{
+		c.name = nullptr;
+	}
+	Cat& operator=(const Cat&& c)
+	{
+		if (&c == this)
+			return *this;
+
+		age = c.age;
+		address = c.address;
+		delete[] name;
+
+		name = c.name;
+		c.name = nullptr;
+
+		return *this;
+	}
+};
+
+int main()
+{
+	Cat c1("nabi", 2);
+
+	Cat c2 = c1; 
+	c2 = c1;	  
+
+	Cat c3 = std::move(c1);
+	c3 = std::move(c2);
+
+}
+
+
+
+
