24fd1b0381cfe7bab80c1f8e7c4da9d96bc11a89
ecourse <smkang@codenuri.co.kr>
2025-12-10 16:14:03 +0900

    Add files via upload

--- DAY3/5_perfect_forwarding1.cpp
+++ DAY3/5_perfect_forwarding1.cpp
@@ -8,16 +8,17 @@ void goo(int& a) { a = 100; }
 template<typename F>
 void chronometry(F f, int arg)
 {
-	f(arg);
+	// <=============== 현재 시간 기록
+	f(arg);			//  함수 호출
+	// <=============== 소요 시간 출력
 }
 
 int main()
 {
 	int n = 10;
 
-	foo(10);
-
-	goo(n);
+	chronometry(foo, 10); // foo(10);
+	chronometry(goo, n);  // goo(n);
 
 	std::cout << n << std::endl;
 }
