2e2a1d9fde8e79b4f328137fdce3580acd1c3fc6
ecourse <smkang@codenuri.co.kr>
2025-12-10 16:27:40 +0900

    Add files via upload

--- DAY3/5_perfect_forwarding3.cpp
+++ DAY3/5_perfect_forwarding3.cpp
@@ -12,10 +12,23 @@ void chronometry(F f, int& arg)
 	f(arg);
 }
 
+
+
+// main					chronometry							hoo(int&&)
+// hoo(10) ===================================================> 10 
+// chronometry(10) ========> int&& arg = 10
+//							hoo(arg) =========================> error
+//							hoo(static_cast<int&&>(arg)) =====> ok
+
 template<typename F>
 void chronometry(F f, int&& arg)
 {
-	f(arg);
+//	f(arg);	// error.
+	f(static_cast<int&&>(arg)); //ok
+
+	// main 에서 rvalue(10) 을 전달했는데.
+	// 받으면서 arg 라는 이름이 생겨서 lvalue 가 된것을
+	// forwarding 할때 다시 rvalue 로 변경해서 전달
 }
 
 int main()
