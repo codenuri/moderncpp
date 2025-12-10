8f93d80f1e93d1b1a45227fe9775dc259bfb4889
ecourse <smkang@codenuri.co.kr>
2025-12-10 10:55:47 +0900

    Add files via upload

---
+++ DAY3/3_move04.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Cat
+{
+	char* name;
+	int   age;
+public:
+	Cat(const char* n, int a) : age(a)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+	}
+	~Cat() { delete[] name; }
+
+	Cat(const Cat& c) : age{ c.age }
+	{
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+	}
+};
+
+Cat foo()
+{
+	Cat c{ "yaong", 3 };
+	return c;
+}
+
+int main()
+{
+	Cat c1{ "nabi", 2 };
+	Cat c2 = c1;  
+
+	Cat c3 = foo(); // 이 코드 실행시 메모리를 잘생각하세요
+					// foo 의 반환값을 생각하세요.. 
+}
