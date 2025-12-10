2cb8bd12a2b2c98675d3161d7cf869cfcb91d3e9
ecourse <smkang@codenuri.co.kr>
2025-12-10 15:55:36 +0900

    Add files via upload

--- DAY3/5_perfect_forwarding0.cpp
+++ DAY3/5_perfect_forwarding0.cpp
@@ -22,12 +22,20 @@ int main()
 	// Point pt{1,1}
 	
 	//				타입				value category
-	//	n			
-	// 10
-	// Point{1,1}
-	// pt
-	// r1
-	// r2
+	//	n			int				lvalue
+	// 10			int				rvalue
+	// Point{1,1}	Point			rvalue
+	// pt			Point			lvalue
+	// r1			int&			lvalue
+	// r2			int&&			lvalue
+
+	foo(r2); // int&
+	foo(static_cast<int&&>(r2)); // int&&
+				// r2 자체가 int&& 타입인데 ?? 다시 int&& 로 캐스팅하네요 ??
+				// => 무슨 의미 인가요 ?
+				// => <> 안에 && 가 있으면 타입 캐스팅이 아닙니다.
+				// => value casting 입니다.
+				// => lvalue => rvalue 로 바꾸는 캐스팅
 }
 
 
