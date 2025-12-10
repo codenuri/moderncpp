a029c6b9b68db9f9272b495438f3c5e2d00c7c56
ecourse <smkang@codenuri.co.kr>
2025-12-10 13:28:00 +0900

    Add files via upload

--- DAY3/3_move08-1.cpp
+++ DAY3/3_move08-1.cpp
@@ -3,6 +3,8 @@
 
 
 // 규칙 #1. 사용자가 복사 계열만 제공할때
+// => 컴파일러는 이동 생성자의 디폴트 구현 제공 안함
+// => std::move() 사용시 사용자가 만든 복사 생성자 사용.
 
 struct Object
 {
@@ -14,7 +16,6 @@ struct Object
 	{
 		std::cout << "사용자가 만든 복사생성자\n";
 	}
-
 };
 
 int main()
@@ -22,8 +23,8 @@ int main()
 	Object o1("obj1");
 	Object o2("obj2");
 
-	Object o3 = o1;
-	Object o4 = std::move(o2);
+	Object o3 = o1;				// 사용자가 만든 복사생성자 사용
+	Object o4 = std::move(o2);  // 사용자가 만든 복사생성자 사용
 
 	std::cout << o1.name << std::endl; 
 	std::cout << o2.name << std::endl; 
