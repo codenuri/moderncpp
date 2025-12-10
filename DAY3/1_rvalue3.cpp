29fed6652d86dae7292ef9b41275eed4059f74b1
ecourse <smkang@codenuri.co.kr>
2025-12-10 09:00:54 +0900

    Add files via upload

--- DAY3/1_rvalue3.cpp
+++ DAY3/1_rvalue3.cpp
@@ -30,8 +30,23 @@ int main()
 	foo() = 5;	// error. "foo()" 라는 표현식은 rvalue
 	goo() = 5;	// ok.    "goo()" 라는 표현식은 lvalue
 
+	// QA#1
 	const int c = 0;
 	c = 10; // error
+			// c 는 lvalue ? rvalue => C언어는 rvalue 로 구분
+			// C++ 은 좀더 많은 조건을 고려해서 결정
+			// => 메모리가 있고, 이름이 있고, 주소를 구할수 있다.
+			// => lvalue 인데, 변경불가능한것
+			// => immutable lvalue 라고 합니다.
+
+	// QA#2. 모든 rvalue 는 상수 이다. YES ? NO ? 
+	// => 답은 NO
+	// => 상수라서 왼쪽에 올수 없는게 아니라
+	// => 문법상(임시로존재하므로) 왼쪽에 올수 없는것
+	Point{ 1,1 }.x = 10; // error
+	Point{ 1,1 }.set(10, 20); // ok. 임시객체라도 멤버 함수로 변경 가능.
+							  // => 곧 파괴되는데 왜 ???
+							  // => move 할때 설명
 }
 
 
