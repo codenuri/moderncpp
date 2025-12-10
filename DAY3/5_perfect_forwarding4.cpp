f37daad1372c9383dbc197765ff1ef24e8357a6a
ecourse <smkang@codenuri.co.kr>
2025-12-10 16:28:44 +0900

    Add files via upload

---
+++ DAY3/5_perfect_forwarding4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void foo(int a) {}
+void goo(int& a) { a = 100; }
+
+void hoo(int&& a) {}
+
+
+template<typename F>
+void chronometry(F f, int& arg)
+{
+	f(arg);
+}
+
+template<typename F>
+void chronometry(F f, int&& arg)
+{
+	f(static_cast<int&&>(arg)); 
+}
+
+int main()
+{
+	int n = 10;
+
+	chronometry(foo, 10); // foo(10);
+	chronometry(goo, n);  // goo(n);
+
+	std::cout << n << std::endl;
+}
