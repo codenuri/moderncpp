b04b023f0997db49a23df42aace52ba6d019c856
ecourse <smkang@codenuri.co.kr>
2025-12-10 11:23:54 +0900

    Add files via upload

---
+++ DAY3/3_move05-2.cpp
@@ -0,0 +1,53 @@
+// std::move()소개
+#include <iostream>
+
+class Cat
+{
+	char* name;
+	int   age;
+public:
+	Cat(const char* n, int a) : age(a)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+	}
+	~Cat() { delete[] name; }
+
+
+	Cat(const Cat& c) : age(c.age)
+	{
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+		std::cout << "복사" << std::endl;
+	}
+
+	Cat(Cat&& c) : age(c.age), name(c.name)
+	{
+		c.name = nullptr;
+		std::cout << "이동" << std::endl;
+	}
+};
+Cat foo()
+{
+	Cat c("yaong", 3);
+	return c;
+}
+
+int main()
+{
+	Cat c1("nabi", 2);
+
+	Cat c2 = c1;			// copy
+	Cat c3 = foo();			// move
+	Cat c4 = static_cast<Cat&&>(c2);	// move	
+	Cat c5 = std::move(c3);	// move
+}
+
+// move 를 공부할때 "확인을 위해서 로깅을 해보면"
+// => 예상과 다르게 출력됩니다.
+// => 컴파일러 최적화 때문에 나타나는 현상
+
+
+
+
+
