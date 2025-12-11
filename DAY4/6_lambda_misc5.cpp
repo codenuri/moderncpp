b1c7e6b317cc176f6e55c1bab7c98d67207955f4
ecourse <smkang@codenuri.co.kr>
2025-12-11 15:56:04 +0900

    Add files via upload

---
+++ DAY4/6_lambda_misc5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+class CompilerGeneratedName
+{
+public:
+	inline int operator()(int a, int b) { return a + b; }
+
+	static int helper(int a, int b)
+	{
+		return a + b;
+	}
+
+	using PF = int(*)(int, int);
+
+	operator PF() { return &CompilerGeneratedName::helper; }
+};
+
+int main()
+{
+	int v1 = 10;
+
+//	int(*f1)(int, int) = [v1](int a, int b) {return a + b + v1; };
+
+	int(*f1)(int, int) = CompilerGeneratedName{};
+
+	std::cout << f1(1, 2) << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
