0f643f40d368dea5a9276318191e0052a750ad4b
ecourse <smkang@codenuri.co.kr>
2025-12-11 16:33:02 +0900

    Add files via upload

--- DAY4/QA5.cpp
+++ DAY4/QA5.cpp
@@ -5,12 +5,17 @@ class Sample
 public:
 	void foo() &  { std::cout << "foo &\n";}
 	void foo() && { std::cout << "foo &&\n"; }
+
+	void operator()()&  { std::cout << "&\n"; }
+	void operator()()&& { std::cout << "&&\n"; }
 };
 
 int main()
 {
 	Sample s;
-	s.foo();
-	
-	Sample{}.foo();
+	s.foo();	// foo() &	
+	Sample{}.foo();	// foo() &&
+
+	s();
+	Sample{}();
 }
