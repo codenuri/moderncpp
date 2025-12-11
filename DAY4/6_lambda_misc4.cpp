8ee2bf228bf8977bfb9625365f90350cf54f6984
ecourse <smkang@codenuri.co.kr>
2025-12-11 15:49:50 +0900

    Add files via upload

---
+++ DAY4/6_lambda_misc4.cpp
@@ -0,0 +1,33 @@
+﻿// 람다 표현식 사용시 컴파일러가 만드는 클래스라고 가정
+class CompilerGeneratedName
+{
+public:
+	inline int operator()(int a, int b) { return a + b; }
+
+
+	using PF = int(*)(int, int);
+
+	operator PF() { return &CompilerGeneratedName::operator(); }
+};
+
+int main()
+{
+	int(*f1)(int, int) = CompilerGeneratedName{};
+						// 임시객체.operator 함수포인터타입()
+
+
+//	int(*f1)(int, int) = [](int a, int b) {return a + b; };
+						// class xxx{}; xxx{};
+}
+
+
+
+
+
+
+
+
+
+
+
+
