e16e42c3a452e384281fbebb9858f57a9fade7ae
ecourse <smkang@codenuri.co.kr>
2025-12-11 15:45:44 +0900

    Add files via upload

--- DAY4/QA3.cpp
+++ DAY4/QA3.cpp
@@ -1,11 +1,18 @@
 class Point
 {
 public:
+	// 변환 연산자함수 : 객체가 다른 타입으로 변환되려고 할때 호출.
+	// 특징 : 함수 이름에 반환 타입 포함. 별도의 반환 타입 표기 안함.
+	operator int()
+	{
+		return 10;
+	}
 };
 
 int main()
 {
 	Point pt;
 
-	int n = pt; // ? ??
+	int n = pt; // Point => int 변환이 필요 하다.
+				// "pt.operator int()" 라는 약속된 함수 호출
 }
