7209939eb024b20858a487b1876963aea38c4b0a
ecourse <smkang@codenuri.co.kr>
2025-12-11 16:54:44 +0900

    Add files via upload

--- DAY4/QA5-2.cpp
+++ DAY4/QA5-2.cpp
@@ -16,7 +16,14 @@ public:
 template<typename F, typename ... T>
 decltype(auto) chronometry(F&& f, T&& ... arg)
 {
-	return f(std::forward<T>(arg)...);
+	// 일반 함수 포인터 : f(인자)
+	// 멤버 함수 포인터 : (객체.*f)(인자)
+	// std::invoke 를 사용하면
+	//std::invoke(일반함수주소, 인자);
+	//std::invoke(멤버함수주소, 객체, 인자);
+
+	//return f(std::forward<T>(arg)...);
+	return std::invoke(std::forward<F>(f), std::forward<T>(arg)...);
 }
 
 int main()
