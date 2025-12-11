c663f051520618db05ee998f4cef16cd7eaf61cf
ecourse <smkang@codenuri.co.kr>
2025-12-12 08:52:18 +0900

    Add files via upload

--- DAY5/1_typename.cpp
+++ DAY5/1_typename.cpp
@@ -21,7 +21,15 @@ template<typename T> void foo(T a)
 
 	// 아래 코드를 생각해 보세요
 	T::data  * p;
-	T::DWORD * p;
+//	T::DWORD * p2; // error. 의도는 포인터를 선언 이지만, 컴파일러는 "값"으로 해석
+
+	typename T::DWORD* p2;
+
+	// "T::이름" : dependent name
+	// => template 인자에 의존해서 얻게 되는 이름
+	// => 컴파일러는 "이름" 을 항상 "값" 으로 해석!
+	// => 타입으로 해석되게 하려면 typename 을 붙여야 한다.
+	// => 단, T가 아닌 "Test::DWORD" 는 typename 붙일필요 없다.
 }
 
 
