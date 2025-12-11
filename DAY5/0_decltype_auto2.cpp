c6cf5ac8b5c901ccd7174715d59a3ddd4925fa99
ecourse <smkang@codenuri.co.kr>
2025-12-12 08:42:58 +0900

    Add files via upload

---
+++ DAY5/0_decltype_auto2.cpp
@@ -0,0 +1,19 @@
+
+int& foo(int& r) { return r; }
+
+
+template<typename F, typename T>
+//auto chronometry(F f, T&& arg)	// return 문장의 결과가 int& 인 경우 int 로 결정, 즉, 참조 제거
+//auto chronometry(F f, T&& arg) -> decltype(f(std::forward<T>(arg))) // ok
+decltype(auto) chronometry(F f, T&& arg)  // ok.  위와 동일
+{
+	return f(std::forward<T>(arg));
+}
+
+
+int main()
+{
+	int n = 3;
+
+	chronometry(foo, n); // foo(n)
+}
