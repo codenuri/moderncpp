c9ec1da3075d5244c656e3ec7b8b283c6c5a5fac
ecourse <smkang@codenuri.co.kr>
2025-12-11 13:24:52 +0900

    Add files via upload

--- DAY4/4_lambda_expression3.cpp
+++ DAY4/4_lambda_expression3.cpp
@@ -17,14 +17,22 @@ int main()
 	int n2 = f1.operator()(1, 2); // 람다 표현식은 결국 함수 객체이므로
 									// 이렇게도 사용 가능.
 	//------------------------------------------------
-	// 다음중 에러는 모두 찾으세요. 그리고 최선의 코드를 선택하세요
+	// 다음중 에러는 모두 찾으세요.  => a2 만 에러.
+	// 그리고 최선의 코드를 선택하세요
 	// 
 
-	auto a1		= [](int a, int b) { return a + b; };
-	auto& a2	= [](int a, int b) { return a + b; };
-	const auto& a3 = [](int a, int b) { return a + b; };
-	auto&& a4	= [](int a, int b) { return a + b; };
+	auto a1		= [](int a, int b) { return a + b; };	 // Point a1 = Point{}; ok
+	auto& a2	= [](int a, int b) { return a + b; };	 // Point& a2 =Point{}; error
+	const auto& a3 = [](int a, int b) { return a + b; }; // const Point& a3 = Point{}; ok
+	auto&& a4	= [](int a, int b) { return a + b; };	 // Point && a4 = Point{}; ok
+														// => 아래 주석 참고
 
+	
+//	auto&& : rvalue reference 일까요 ? forwarding reference 일까요 ??
+//			=> forwarding reference !! T&& 와 동일
+
+//	auto&& b1 = n; // ok   int& b1 = n;
+//	auto&& b2 = 3; // ok   int&& b2 = 3;
 
 
 }
