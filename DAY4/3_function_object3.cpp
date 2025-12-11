65d42ce16e223c0a28fdce74c24e0fbcb41a5926
ecourse <smkang@codenuri.co.kr>
2025-12-11 10:48:23 +0900

    Add files via upload

--- DAY4/3_function_object3.cpp
+++ DAY4/3_function_object3.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <bitset>
 
+// 함수 객체
+// => 동작 뿐 아니라, 상태를 가진다(멤버 데이타가 있다는 의미)
+// => 생성자/소멸자 등도 활용 가능하다.
 
 class URandom
 {
+	std::bitset<10> bs; // 10 개 비트를 관리하는 STL
+	bool recycle;
 public:
+	URandom(bool b = false) : recycle{ b }
+	{
+//		bs.set(3); // 3번째 비트만 1로
+		bs.set();  // 모든 비트를 1로
+	}
 	int operator()()
 	{
 		return rand() % 10;
