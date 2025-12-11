eeef6869ced92e060e850d9f7486f0a77ebe4a98
ecourse <smkang@codenuri.co.kr>
2025-12-11 15:29:25 +0900

    Add files via upload

--- DAY4/6_lambda_misc2-1.cpp
+++ DAY4/6_lambda_misc2-1.cpp
@@ -1,13 +1,32 @@
 // C++14 부터 람다표현식 인자로 auto 사용가능합니다.
 
 // 일반 함수 인자도 auto 될까요 ?
+// => C++20 부터 됩니다.
+// => 원리는 템플림
+void foo(auto n)		// template<typename T> 
+{						// void foo(T n)
+}
 
-void foo(auto n)
-{
+auto add(auto a, auto b)  // template<typename T1, typename T2>
+{						  // auto add(T1 a, T2 b)
+	return a + b;
+}
+
+#include <concepts>
+auto add2(std::integral auto a, std::integral  auto b)
+{						  
+	return a + b;
 }
 
 int main()
 {
 	foo(3);
 	foo(3.4);
+
+	auto n1 = add(1, 2);
+	auto n2 = add(1.1, 2.2);
+	auto n3 = add(1.1, 2);
+	auto n4 = add2(1, 2); // ok
+	auto n5 = add2(1.1, 2.2); // error
 }
+
