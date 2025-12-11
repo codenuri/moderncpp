2e3820c622ea1ee77584117e5053b47418b76c61
ecourse <smkang@codenuri.co.kr>
2025-12-11 09:10:39 +0900

    Add files via upload

--- DAY4/1_using_perfect_forwarding3.cpp
+++ DAY4/1_using_perfect_forwarding3.cpp
@@ -25,7 +25,14 @@ int main()
 	// 아래 코드는 동적 메모리를 몇번 할당 할까요 ? 2번
 	// #1. 사용자 코드의 "new Point(1,2)" 에서 8 바이트 할당
 	// #2. shared_ptr 의 생성자에서 "new control block" 할당 - 24 ~ 30 정도
-	std::shared_ptr<Point> sp1( new Point(1,2) );
+	//std::shared_ptr<Point> sp1( new Point(1,2) );
+
+
+	// #2. std::make_shared
+	// => sizeof(Point) + sizeof(Controlbock) 를 한번에 할당
+	// => 메모리 파편화를 막고, 메모리 크기도 줄어든다.
+	//    (같이 할당하면 정보가 줄어 든다.)
+	std::shared_ptr<Point> sp1 = std::make_shared<Point>(1, 2);
 }
 
 
