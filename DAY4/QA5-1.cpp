9d541b45731d92e526e07989a5b245e353801146
ecourse <smkang@codenuri.co.kr>
2025-12-11 16:38:23 +0900

    Add files via upload

--- DAY4/QA5-1.cpp
+++ DAY4/QA5-1.cpp
@@ -8,15 +8,15 @@ public:
 };
 
 template<typename F, typename ... T>
-decltype(auto) chronometry(F f, T&& ... arg)
+decltype(auto) chronometry(F&& f, T&& ... arg)
 {
-	return f(std::forward<T>(arg)...);
+	return std::forward<F>(f)(std::forward<T>(arg)...);
 }
 int main()
 {
 	Sample s;
-	chronometry(s);
-	chronometry(Sample{});
+	chronometry(s);			// operator() & 성능 측정
+	chronometry(Sample{});	// operator() && 성능 측정
 }
 
 
