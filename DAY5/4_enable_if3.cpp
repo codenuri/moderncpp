f4b61fe03fb13b2de0f8dd54c4e530531fe0451f
ecourse <smkang@codenuri.co.kr>
2025-12-12 11:05:10 +0900

    Add files via upload

--- DAY5/4_enable_if3.cpp
+++ DAY5/4_enable_if3.cpp
@@ -2,15 +2,29 @@
 #include <type_traits>
 
 // enable_if<조건, 타입>::type
-//          true          ?
-//          false         ?
+//          true          타입    "타입생략시는 void"
+//          false         ::type 자체가 없음. 
+
+// 결론
+// => 반환 타입위치에 enable_if 를 사용하는 기술
+// std::enable_if_t< 조건, "타입" > : 조건이 true 이면 반환 타입은 "타입"
+//									 조건이 false 라면 반환 타입 만들수없음.
+//									=> SFINAE 에 의해 에러 아님. 
+//									=> 후보에서만 제외
+// "타입" 생략시 void
 
 template<typename T> 
-std::enable_if_t< std::is_integral_v<T>, void >
+
+//typename std::enable_if< std::is_integral_v<T>, T >::type // C++11 스타일
+
+std::enable_if_t< std::is_integral_v<T>, T > // C++14 스타일
 gcd(T a, T b)
 {
 	return 0;
 }
+
+
+
 
 double gcd(double, double)
 {
