2c85e60db6724d3180fb341e3ba9abf2b52647db
ecourse <smkang@codenuri.co.kr>
2025-12-12 10:59:24 +0900

    Add files via upload

---
+++ DAY5/4_enable_if3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// enable_if<조건, 타입>::type
+//          true          ?
+//          false         ?
+
+template<typename T> 
+std::enable_if_t< std::is_integral_v<T>, void >
+gcd(T a, T b)
+{
+	return 0;
+}
+
+double gcd(double, double)
+{
+	std::cout << "실수일때 다른 기술\n";
+}
+
+int main()
+{
+	gcd(6, 9);		
+	gcd(3.1, 5.4);	
+	gcd(3.1f, 5.4f);
+}
