89807732ab51c56e711b1c4db088b350105b9658
ecourse <smkang@codenuri.co.kr>
2025-12-12 13:17:53 +0900

    Add files via upload

--- DAY5/5_variadic_template3.cpp
+++ DAY5/5_variadic_template3.cpp
@@ -13,7 +13,26 @@ template<typename ... Types> void foo(Types ... args)
 	std::cout << sizeof...(Types) << std::endl;
 	std::cout << sizeof...(args ) << std::endl;
 
+
+	// #3. pack expansion - 핵심
+	// "pack이름을 사용하는패턴"... => E1패턴, E2패턴, E3패턴
+
+//	goo(args);	// error. pack 이름을 인자로 직접 사용안됨
+
+	goo(args...); // ok. pack 의 모든 요소를 , 를 사용해서 열거
+				  // goo(E1, E2, E3)
+
+	goo((++args)...); // goo(++E1, ++E2, ++E3)
+
+
+	goo(hoo(args...)); // goo(hoo(E1, E2, E3)) 의미.. 
+						// 그런데 hoo() 는 인자 한개..
+						// error
+
+	? ; // ok.  goo(hoo(E1), hoo(E2), hoo(E3)) 
+
 }
+
 
 
 int main()
