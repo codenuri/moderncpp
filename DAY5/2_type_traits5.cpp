63147c3e6bc22c7383c0adb988065c0945236d42
ecourse <smkang@codenuri.co.kr>
2025-12-12 09:48:26 +0900

    Add files via upload

--- DAY5/2_type_traits5.cpp
+++ DAY5/2_type_traits5.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include <type_traits>
 
+// 참고 : C++ 표준
+// remove_pointer 는 있지만 remove_all_pointer 는 없음. 
+
 template<typename T> struct remove_all_pointer
 {
 	using type = T;
@@ -8,7 +11,9 @@ template<typename T> struct remove_all_pointer
 
 template<typename T> struct remove_all_pointer<T*>
 {
-	using type = T;
+	// 컴파일 시간 재귀를 사용해서 모든 포인터 제거
+	// => 재귀의 종료는 "T 가 더이상 포인터가 아닌 경우", primary 사용하므로 종료!
+	using type = typename remove_all_pointer<T>::type ;
 };
 
 int main()
