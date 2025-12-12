e9392ea5b4e1e1b73d0e17988aa4e3b6d764e581
ecourse <smkang@codenuri.co.kr>
2025-12-12 09:44:03 +0900

    Add files via upload

---
+++ DAY5/2_type_traits5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template<typename T> struct remove_all_pointer
+{
+	using type = T;
+};
+
+template<typename T> struct remove_all_pointer<T*>
+{
+	using type = T;
+};
+
+int main()
+{
+	remove_all_pointer<int***>::type n;
+}
