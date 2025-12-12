3826d9ffb2d30ddda102b003c2b14b1af45fb97c
ecourse <smkang@codenuri.co.kr>
2025-12-12 10:01:26 +0900

    Add files via upload

---
+++ DAY5/2_type_traits8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <type_traits>
+
+int main()
+{
+	int n = 10;
+
+	(n + 3) = 10;	// error. n + 3 은 rvalue
+	(n = 3) = 10;   // ok     n = 3 은 lvalue
+
+	// decltype(expr) : 규칙 2. expr 이 lvalue 라면 참조 타입, 아니면 값 타입
+	
+	
+
+	if (std::is_lvalue_reference_v<  decltype( (n = 3) )  > == true)
+	{
+			std::cout << "lvalue\n";
+	}
+
+}
