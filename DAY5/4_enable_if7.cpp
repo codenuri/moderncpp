5c1aabe13a9809f977512440a5b7e397b221822b
ecourse <smkang@codenuri.co.kr>
2025-12-12 11:25:52 +0900

    Add files via upload

--- DAY5/4_enable_if7.cpp
+++ DAY5/4_enable_if7.cpp
@@ -3,12 +3,27 @@
 #include <concepts>
 
 // std::is_integral_v<T> : T 가 정수인지 조사하는 traits
-// std::integral<T>      : T 가 정수인지 조사하는 concept 이라는 새로운기술
+// std::integral<T>      : T 가 정수인지 조사하는 concept 이라는 새로운기술(C++20)
 
+/*
 template<typename T> requires std::integral<T>
 void foo(T a)
 {
 }
+*/
+/*
+// 위와 동일
+template<std::integral T> 
+void foo(T a)
+{
+}
+*/
+
+// 위와 동일
+void foo(std::integral auto a)
+{
+}
+
 
 int main()
 {
