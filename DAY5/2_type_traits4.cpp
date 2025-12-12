46ef591fa9f32820afe73a76615881ff695cb368
ecourse <smkang@codenuri.co.kr>
2025-12-12 09:37:49 +0900

    Add files via upload

---
+++ DAY5/2_type_traits4.cpp
@@ -0,0 +1,32 @@
+﻿// 4_type_traits3 - 210 page
+#include <iostream>
+#include <type_traits>
+
+// traits 의 종류
+// 1. 타입질의 : is_xxx<T>::value
+// 2. 변형타입얻기 : xxx<T>::type
+
+
+template<typename T> struct remove_pointer
+{
+	using type = T;
+};
+
+
+
+
+int main()
+{
+	remove_pointer<int*>::type n2;  // int n2
+}
+
+
+
+
+
+
+
+
+template<typename T> void foo(T a)
+{
+}
