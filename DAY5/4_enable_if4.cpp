38486f96ea11d79b32cc0facf212133a9d7b5266
ecourse <smkang@codenuri.co.kr>
2025-12-12 11:11:47 +0900

    Add files via upload

--- DAY5/4_enable_if4.cpp
+++ DAY5/4_enable_if4.cpp
@@ -12,18 +12,34 @@ void foo(double) {}
 //void goo(가상함수가없는타입) {}
 
 template<typename T>
-void goo(T)
+
+//std::enable_if_t< 조건, 반환타입>
+//std::enable_if_t< std::is_polymorphic_v<T>, void>
+std::enable_if_t< std::is_polymorphic_v<T>>
+goo(T)
 {
 	std::cout << "가상함수가 있는 타입\n";
 }
+
 template<typename T>
+std::enable_if_t< !std::is_polymorphic_v<T>>
 void goo(T)
 {
 	std::cout << "가상함수가 없는 타입\n";
 }
 
+class A
+{
+public:
+	virtual void foo() {}
+};
 int main()
 {
+	A a;
+	int n;
+
+	goo(a); // 가상함수 있는 타입
+	goo(n); // 가상함수 없는 타입
 
 }
 
