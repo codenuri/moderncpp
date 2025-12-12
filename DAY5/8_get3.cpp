f2100b961704b1b50da32054127b8cadb2ad2d6a
ecourse <smkang@codenuri.co.kr>
2025-12-12 15:21:24 +0900

    Add files via upload

--- DAY5/8_get3.cpp
+++ DAY5/8_get3.cpp
@@ -22,18 +22,61 @@ struct tuple<T, Types...> : public tuple<Types...>
 	static constexpr int N = base::N + 1;
 };
 // =============================================================
+// primary template
+template<int N, typename TP>
+struct tuple_element
+{
+//	using type = 튜플 TP 에서 N 번째 요소의 타입을 type;
+	
+	// primary 버전에서는 구할수 없습니다.
+	// "구할수 있게 부분 특수화를 만드는 것이 핵심"
+};
+
+// N == 0 일때의 부분 특수화
+/*
+template<typename TP>
+struct tuple_element<0, TP>
+{
+	using type = TP의 0번째요소타입;
+
+	// 아직 구할수 없다. 잘못된 부분특수화
+};
+
+template<typename ... Types>
+struct tuple_element<0, tuple<Types...> >
+{
+	using type = tuple<Types...> 의 0번째요소타입;
+
+	// 역시 못 구함. 잘못된 부분 특수화
+};
+*/
+template<typename T, typename ... Types>
+struct tuple_element<0, tuple<T, Types...> >
+{
+	using type = T;
+
+};
+
+
+
+
+
+
 
 template<typename TP>
 void foo(TP& tp)
 {
 	// 현재 TP : tuple<int, double, short>
 
-	typename tuple_element<1, TP>::type d; // 만들어 봅시다.
+	typename tuple_element<0, TP>::type d; // 만들어 봅시다.
 
-	std::cout << typeid(d).name() << std::endl; // double
+	std::cout << typeid(d).name() << std::endl; // int
 }
+
 
 int main()
 {
 	tuple<int, double, short> t{ 1, 3.4, 'A' };
+
+	foo(t);
 }
