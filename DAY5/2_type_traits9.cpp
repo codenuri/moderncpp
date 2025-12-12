118fcf2d441818d46901195fbd25228c4553751d
ecourse <smkang@codenuri.co.kr>
2025-12-12 10:06:48 +0900

    Add files via upload

--- DAY5/2_type_traits9.cpp
+++ DAY5/2_type_traits9.cpp
@@ -8,8 +8,14 @@ public:
 	Object(Object&& obj)      { std::cout << "move\n"; }
 };
 
+// 핵심
+// T&  : lvalue 만 받을수 있다.
+// T&& : lvalue, rvalue 를 모두 받을수 있다.
+
+// std::move : lvalue, rvalue 를 모두 받아서, rvalue 캐스팅
+
 template<typename T>
-T&& mymove(T& obj)
+T&& mymove(T&& obj)
 {
 	return static_cast<T&&>(obj);
 }
@@ -20,6 +26,8 @@ int main()
 
 	Object o2 = std::move(o1);
 				// static_cast<Object&&>(o1)
+	Object o3 = std::move(Object{});
 
-	Object o3 = mymove(o2);	// 만들어 봅시다.
+	Object o4 = mymove(o2);	
+	Object o5 = mymove(Object{});
 }
