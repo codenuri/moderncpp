18f4d15129ba138c8f7616dfe1e4bd5d6ac0ee9a
ecourse <smkang@codenuri.co.kr>
2025-12-12 10:20:11 +0900

    Add files via upload

--- DAY5/2_type_traits7.cpp
+++ DAY5/2_type_traits7.cpp
@@ -3,18 +3,23 @@
 
 template<typename T> void printv(const T& v)
 {
-	if (   )
+	// if : 조건이 false 라면 실행되지 않지만, template instantiation 에는 포함됨
+	//      => 생성된 함수에 코드가 포함된다는 의미
+
+	// if constexpr : 조건이 false 라면 template instantiation 에는 포함 하지 말라
+	//      => C++17 기술
+	if constexpr ( std::is_pointer_v<T>  )
 		std::cout << v << " : " << *v << std::endl;
 	else
 		std::cout << v << std::endl;
 }
+
+
 int main()
 {
 	int n = 10;
-	double d = 3.4;
 
-	printv(n);
-	printv(d);
+	printv(n); // !!
 	printv(&n);
 }
 
