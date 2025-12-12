4c4cbec9d1f855e92debd0a8d35e9e73959e615d
ecourse <smkang@codenuri.co.kr>
2025-12-12 10:15:30 +0900

    Add files via upload

---
+++ DAY5/2_type_traits7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template<typename T> void printv(const T& v)
+{
+	if (   )
+		std::cout << v << " : " << *v << std::endl;
+	else
+		std::cout << v << std::endl;
+}
+int main()
+{
+	int n = 10;
+	double d = 3.4;
+
+	printv(n);
+	printv(d);
+	printv(&n);
+}
+
