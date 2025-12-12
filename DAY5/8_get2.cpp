ab0ef014231803085bad2983fbfb42d1526f25d5
ecourse <smkang@codenuri.co.kr>
2025-12-12 14:54:16 +0900

    Add files via upload

--- DAY5/8_get2.cpp
+++ DAY5/8_get2.cpp
@@ -34,5 +34,35 @@ int main()
 
 	std::cout << static_cast<tuple<double, short>&>(t).value << std::endl; // 3.4
 
-	std::cout << static_cast<tuple<short>&>(t).value << std::endl; // 'A'
+	std::cout << static_cast<tuple<short>&>(t).value << std::endl; // 65
+
+
+	double d = get<1>(t);
+
+//	get<1>(t) = 1.1;
 }
+
+// get 의 모양
+template<int N, typename TP> 
+튜플TP의 N번째 요소의 타입&
+get(TP& tp)
+{
+	return static_cast<TP의N번째부모의타입&>(tp).value;
+}
+
+// 결국 get을 구현하려면 2개를 알아야 합니다.
+// TP 가 튜플 일때
+
+// 1. TP 의 N 번째 요소의 타입
+// 2. TP 의 N 번째 부모의 타입
+
+// => 결국 타입구하기가 핵심
+
+
+
+
+
+
+
+
+
