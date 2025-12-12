bb291cec857167d49e3c713fd9d6aa2dce640b21
ecourse <smkang@codenuri.co.kr>
2025-12-12 11:40:48 +0900

    Add files via upload

---
+++ DAY5/5_variadic_template1.cpp
@@ -0,0 +1,14 @@
+﻿// 7_가변인자템플릿1 - 225 page
+
+// C++11 가변인자 템플릿
+// => 타입 인자 갯수에 제한이 없다
+template<typename ... Types> class tuple 
+{
+};
+
+int main()
+{
+	tuple<> t0;
+	tuple<int> t1;
+	tuple<int, double> t2; // Types : int, double 
+}
