70dc92286ff4d4b4d9b121d41ea313f5c59919ca
ecourse <smkang@codenuri.co.kr>
2025-12-12 11:22:30 +0900

    Add files via upload

---
+++ DAY5/4_enable_if7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <type_traits>
+#include <concepts>
+
+// std::is_integral_v<T> : T 가 정수인지 조사하는 traits
+// std::integral<T>      : T 가 정수인지 조사하는 concept 이라는 새로운기술
+
+template<typename T> requires std::integral<T>
+void foo(T a)
+{
+}
+
+int main()
+{
+	foo(3); // ok
+	foo(3.4); // error. 조건 만족 못함
+}
